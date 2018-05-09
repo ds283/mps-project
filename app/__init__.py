1fcff0883dd8712af6867584f0c16f129b1fb3dc
ds283 <D.Seery@sussex.ac.uk>
2018-05-09 20:48:31 +0100

    Implement basic user creation, currently transplanted from flask-security
    - doesn’t fill in the richer metadata that we want to capture, such as first/last name, username and role

--- app/__init__.py
+++ app/__init__.py
@@ -49,4 +49,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
     return app
---
+++ app/admin/actions.py
@@ -0,0 +1,40 @@
+#
+# Created by David Seery on 09/05/2018.
+# Copyright (c) 2018 University of Sussex. All rights reserved.
+#
+# This file is part of the MPS-Project platform developed in
+# the School of Mathematics & Physical Sciences, University of Sussex.
+#
+# Contributors: David Seery <D.Seery@sussex.ac.uk>
+#
+
+
+from flask import current_app, render_template, redirect, url_for, flash
+from werkzeug.local import LocalProxy
+from flask_security.utils import hash_password, do_flash, config_value, send_mail, get_message
+from flask_security.signals import user_registered
+from flask_security.confirmable import generate_confirmation_link
+
+
+_security = LocalProxy(lambda: current_app.extensions['security'])
+_datastore = LocalProxy(lambda: _security.datastore)
+
+
+def register_user(**kwargs):
+    confirmation_link, token = None, None
+    kwargs['password'] = hash_password(kwargs['password'])
+    user = _datastore.create_user(**kwargs)
+    _datastore.commit()
+
+    if _security.confirmable:
+        confirmation_link, token = generate_confirmation_link(user)
+        do_flash(*get_message('CONFIRM_REGISTRATION', email=user.email))
+
+    user_registered.send(app._get_current_object(),
+                         user=user, confirm_token=token)
+
+    if config_value('SEND_REGISTER_EMAIL'):
+        send_mail(config_value('EMAIL_SUBJECT_REGISTER'), user.email,
+                  'welcome', user=user, confirmation_link=confirmation_link)
+
+    return user

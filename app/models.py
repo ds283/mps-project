cd0b8e782ff9d18a76574867207fcc6053d76e6c
ds283 <D.Seery@sussex.ac.uk>
2018-05-13 12:10:58 +0100

    Implement active/inactive flags for research groups

--- app/models.py
+++ app/models.py
@@ -97,3 +97,5 @@ class ResearchGroup(db.Model):
 
     abbreviation = db.Column(db.String(DEFAULT_STRING_LENGTH), index=True, unique=True)
     name = db.Column(db.String(DEFAULT_STRING_LENGTH))
+
+    active = db.Column(db.Boolean())

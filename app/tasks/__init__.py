ef2077fb74da1d030a14b5d9a7e6f4384831dbd4
ds283 <D.Seery@sussex.ac.uk>
2018-06-01 17:52:14 +0100

    Begin implementing backup task

---
+++ app/tasks/__init__.py
@@ -0,0 +1,13 @@
+#
+# Created by David Seery on 01/06/2018.
+# Copyright (c) 2018 University of Sussex. All rights reserved.
+#
+# This file is part of the MPS-Project platform developed in
+# the School of Mathematics & Physical Sciences, University of Sussex.
+#
+# Contributors: David Seery <D.Seery@sussex.ac.uk>
+#
+
+from .send_log_email import register_send_log_email
+from .prune_email import register_prune_email
+from .backup import register_backup_tasks

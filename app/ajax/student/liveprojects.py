df28d5fdd9648748a88cc2a36090b1296e368acf
ds283 <D.Seery@sussex.ac.uk>
2018-06-15 11:27:46 +0100

    Fix issues with student dashboard

--- app/ajax/student/liveprojects.py
+++ app/ajax/student/liveprojects.py
@@ -13,26 +13,34 @@ from flask import render_template_string, jsonify, url_for
 
 _available = \
 """
-{% if project.meeting_reqd == project.MEETING_REQUIRED %}
-    <span class="label label-danger">Meeting required</span>
-{% elif project.meeting_reqd == project.MEETING_OPTIONAL %}
-    <span class="label label-warning">Meeting optional</span>
-{% else %}
-    <span class="label label-default">Meeting not required</span>
-{% endif %}
-{% if project.is_available(sel) %}
-    <span class="label label-success">Available</span>
-{% else %}
-    {% if sel in project.confirm_waiting %}
-        <a href="{{ url_for('student.cancel_confirmation', sid=sel.id, pid=project.id) }}" class="btn btn-default btn-sm table-button">
-            Cancel
-        </a>
-    {% else %}
-        <a href="{{ url_for('student.request_confirmation', sid=sel.id, pid=project.id) }}" class="btn btn-default btn-sm table-button">
-            Request
-        </a>
-    {% endif %}
-{% endif %}
+<div class="row">
+    <div class="col-xs-7">
+        {% if project.meeting_reqd == project.MEETING_REQUIRED %}
+            <span class="label label-danger">Meeting required</span>
+        {% elif project.meeting_reqd == project.MEETING_OPTIONAL %}
+            <span class="label label-warning">Meeting optional</span>
+        {% else %}
+            <span class="label label-default">Meeting not required</span>
+        {% endif %}
+    </div>
+    <div class="col-xs-5">
+        <div class="pull-right">
+            {% if project.is_available(sel) %}
+                <span class="label label-success">Available</span>
+            {% else %}
+                {% if sel in project.confirm_waiting %}
+                    <a href="{{ url_for('student.cancel_confirmation', sid=sel.id, pid=project.id) }}" class="btn btn-warning btn-sm table-button">
+                        Cancel
+                    </a>
+                {% else %}
+                    <a href="{{ url_for('student.request_confirmation', sid=sel.id, pid=project.id) }}" class="btn btn-default btn-sm table-button">
+                        Request
+                    </a>
+                {% endif %}
+            {% endif %}
+        </div>
+    </div>
+</div>
 """
 
 _bookmarks = \
@@ -117,9 +125,9 @@ def liveprojects_data(sel):
              'name': '<a href="{url}">{name}</strong></a>'.format(name=p.name,
                                                                   url=url_for('student.view_project', sid=sel.id,
                                                                               pid=p.id)),
-             'supervisor': '{name} <a href="mailto:{em}>{em}</a>'.format(name=p.owner.build_name(),
+             'supervisor': '{name} <a href="mailto:{em}">{em}</a>'.format(name=p.owner.build_name(),
                                                                          em=p.owner.email),
-             'group': p.group.make_label(p.group_name),
+             'group': p.group.make_label(p.group.name),
              'available': render_template_string(_available, sel=sel, project=p),
              'bookmarks': render_template_string(_bookmarks, sel=sel, project=p),
              'menu': render_template_string(_menu, sel=sel, project=p)} for p in sel.config.live_projects]

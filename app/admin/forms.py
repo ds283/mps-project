148e4b065d44b105072a4e8493bbf3ce8359c9ac
ds283 <D.Seery@sussex.ac.uk>
2018-07-17 13:58:29 +0100

    Add scheduled task to thin LiveProject popularity data

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -681,7 +681,8 @@ class ScheduledTaskMixin():
                        ('app.tasks.backup.limit_size', 'Enforce limit on size of backup folder'),
                        ('app.tasks.backup.clean_up', 'Clean up backup folder'),
                        ('app.tasks.backup.drop_absent_backups', 'Drop absent backups'),
-                       ('app.tasks.popularity.update_popularity_data', 'Update LiveProject popularity data')]
+                       ('app.tasks.popularity.update_popularity_data', 'Update LiveProject popularity data'),
+                       ('app.tasks.popularity.thin', 'Thin LiveProject popularity data')]
                        # ('remote_backup', 'Backup to internet location')]
     task = SelectField('Task', choices=tasks_available)
 

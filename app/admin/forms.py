1bf79c1d47b3f97bd4fad5de227d2f81e0b67436
ds283 <D.Seery@sussex.ac.uk>
2018-08-02 13:00:37 +0100

    Small reorganization of WTForms material

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -8,312 +8,29 @@
 # Contributors: David Seery <D.Seery@sussex.ac.uk>
 #
 
-from flask import request, current_app
-from flask_security import current_user
+from flask import request
 from flask_security.forms import Form, RegisterFormMixin, UniqueEmailFormMixin, NextFormMixin, get_form_field_label
 from flask_security.forms import password_length, email_required, email_validator, EqualTo
-from werkzeug.local import LocalProxy
 from wtforms import StringField, IntegerField, SelectField, PasswordField, BooleanField, SubmitField, \
-    TextAreaField, ValidationError, DateTimeField, FloatField, RadioField
+    TextAreaField, DateTimeField, FloatField, RadioField
 from wtforms.validators import DataRequired, Optional
 from wtforms_alchemy.fields import QuerySelectField, QuerySelectMultipleField
 
-from ..models import db, User, Role, FacultyData, ResearchGroup, DegreeType, DegreeProgramme, TransferableSkill, \
-    ProjectClass, Supervisor, BackupConfiguration, EnrollmentRecord, SkillGroup, \
-    submission_choices, academic_titles, extent_choices, year_choices
-
-from ..fields import EditFormMixin, CheckboxQuerySelectMultipleField
-
-from usernames import is_safe_username
-from zxcvbn import zxcvbn
-
-import json
-
-
-_security = LocalProxy(lambda: current_app.extensions['security'])
-_datastore = LocalProxy(lambda: _security.datastore)
-
-
-def valid_username(form, field):
-    if not is_safe_username(field.data):
-        raise ValidationError('User name "{name}" is not valid'.format(name=field.data))
-
-
-def globally_unique_username(form, field):
-    if _datastore.get_user(field.data) is not None:
-        raise ValidationError('{name} is already associated with an account'.format(name=field.data))
-
-
-def unique_or_original_username(form, field):
-    if field.data != form.user.username and _datastore.get_user(field.data) is not None:
-        raise ValidationError('{name} is already associated with an account'.format(name=field.data))
-
-
-def existing_username(form, field):
-    user = _datastore.get_user(field.data)
-
-    if user is None:
-        raise ValidationError('userid {name} is not an existing user'.format(name=field.data))
-    if not user.is_active:
-        raise ValidationError('userid {name} exists, but it not currently active'.format(name=field.data))
-
-
-def unique_or_original_email(form, field):
-    if field.data != form.user.email and _datastore.get_user(field.data) is not None:
-        raise ValidationError('{name} is already associated with an account'.format(name=field.data))
-
-
-def globally_unique_group_abbreviation(form, field):
-    if ResearchGroup.query.filter_by(abbreviation=field.data).first():
-        raise ValidationError('{name} is already associated with a research group'.format(name=field.data))
-
-
-def unique_or_original_abbreviation(form, field):
-    if field.data != form.group.abbreviation and ResearchGroup.query.filter_by(abbreviation=field.data).first():
-        raise ValidationError('{name} is already associated with a research group'.format(name=field.data))
-
-
-def globally_unique_degree_type(form, field):
-    if DegreeType.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a degree type'.format(name=field.data))
-
-
-def unique_or_original_degree_type(form, field):
-    if field.data != form.degree_type.name and DegreeType.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a degree type'.format(name=field.data))
-
-
-def globally_unique_degree_programme(form, field):
-    degree_type = form.degree_type.data
-    if DegreeProgramme.query.filter_by(name=field.data, type_id=degree_type.id).first():
-        raise ValidationError('{name} is already associated with a degree programme of the same type'.format(name=field.data))
-
-
-def unique_or_original_degree_programme(form, field):
-    degree_type = form.degree_type.data
-    if (field.data != form.programme.name or degree_type.id != form.programme.type_id) and \
-            DegreeProgramme.query.filter_by(name=field.data, type_id=degree_type.id).first():
-        raise ValidationError('{name} is already associated with a degree programme of the same type'.format(name=field.data))
-
-
-def globally_unique_transferable_skill(form, field):
-    if TransferableSkill.query.filter(TransferableSkill.name == field.data,
-                                      TransferableSkill.group_id == form.group.data.id).first():
-        raise ValidationError('{name} is already associated with a transferable skill'.format(name=field.data))
-
-
-def unique_or_original_transferable_skill(form, field):
-    if field.data != form.skill.name and \
-            TransferableSkill.query.filter(TransferableSkill.name == field.data,
-                                           TransferableSkill.group_id == form.group.data.id).first():
-        raise ValidationError('{name} is already associated with a transferable skill'.format(name=field.data))
-
-
-def globally_unique_skill_group(form, field):
-    if SkillGroup.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a skill group'.format(name=field.data))
-
-
-def unique_or_original_skill_group(form, field):
-    if field.data != form.group.name and SkillGroup.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a skill group'.format(name=field.data))
-
-
-def globally_unique_project_class(form, field):
-    if ProjectClass.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a project class'.format(name=field.data))
-
-
-def unique_or_original_project_class(form, field):
-    if field.data != form.project_class.name and ProjectClass.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a project class'.format(name=field.data))
-
-
-def globally_unique_project_class_abbrev(form, field):
-    if ProjectClass.query.filter_by(abbreviation=field.data).first():
-        raise ValidationError('{name} is already in use as an abbreviation'.format(name=field.data))
-
-
-def unique_or_original_project_class_abbrev(form, field):
-    if field.data != form.project_class.abbreviation and ProjectClass.query.filter_by(abbreviation=field.data).first():
-        raise ValidationError('{name} is already in use as an abbreviation'.format(name=field.data))
-
-
-def globally_unique_supervisor(form, field):
-    if Supervisor.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a supervisory role'.format(name=field.data))
-
-
-def unique_or_original_supervisor(form, field):
-    if field.data != form.supervisor.name and Supervisor.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a supervisory role'.format(name=field.data))
-
-
-def globally_unique_role(form, field):
-    if Role.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a user role'.format(name=field.data))
-
-
-def unique_or_original_role(form, field):
-    if field.data != form.role.name and Role.query.filter_by(name=field.data).first():
-        raise ValidationError('{name} is already associated with a user role'.format(name=field.data))
-
-
-def valid_json(form, field):
-    try:
-        json_obj = json.loads(field.data)
-    except TypeError:
-        raise ValidationError('Unexpected text encoding')
-    except json.JSONDecodeError:
-        raise ValidationError('Could not translate to a valid JSON object')
-
-
-def password_strength(form, field):
-
-    username = form.username.data or ''
-    first_name = form.first_name.data or ''
-    last_name = form.last_name.data or ''
-
-    # password length validation doesn't stop the validation chain if the password is too short
-    # in this case, just exit because validating the password doesn't make sense
-    if len(field.data) < 6:
-        return
-
-    results = zxcvbn(field.data, user_inputs=[username, first_name, last_name])
-
-    if 'score' in results and int(results['score']) <= 2:
-
-        msg = ''
-        if 'feedback' in results:
-            if 'warning' in results['feedback']:
-                msg = results['feedback']['warning']
-                if msg is not None and len(msg) > 0 and msg[-1] != '.':
-                    msg += '.'
-
-        if len(msg) is 0:
-            msg = 'Weak password (score {n}).'.format(n=results['score'])
-
-        if 'feedback' in results:
-            if 'suggestions' in results['feedback'] is not None:
-                for m in results['feedback']['suggestions']:
-                    msg = msg + " " + m
-                    if msg[-1] != '.':
-                        msg += '.'
-
-        if 'crack_times_display' in results:
-
-            if 'online_no_throttling_10_per_second' in results['crack_times_display']:
-
-                msg = msg + " Estimated crack time: " + results['crack_times_display']['online_no_throttling_10_per_second']
-                if msg[-1] != '.':
-                    msg += '.'
-
-        raise ValidationError(msg)
-
-
-class OptionalIf(Optional):
-    """
-    Makes a field optional if another field is set true
-    """
-
-    def __init__(self, other_field_name, *args, **kwargs):
-
-        self.other_field_name = other_field_name
-        super(OptionalIf, self).__init__(*args, **kwargs)
-
-
-    def __call__(self, form, field):
-
-        other_field = form._fields.get(self.other_field_name)
-
-        if other_field is None:
-            return
-
-        if bool(other_field.data):
-            super(OptionalIf, self).__call__(form, field)
-
-
-class NotOptionalIf(Optional):
-    """
-    Makes a field optional if another field is set false
-    """
-
-    def __init__(self, other_field_name, *args, **kwargs):
-
-        self.other_field_name = other_field_name
-        super(NotOptionalIf, self).__init__(*args, **kwargs)
-
-
-    def __call__(self, form, field):
-
-        other_field = form._fields.get(self.other_field_name)
-
-        if other_field is None:
-            return
-
-        if not bool(other_field.data):
-            super(NotOptionalIf, self).__call__(form, field)
-
-
-def GetActiveDegreeTypes():
-
-    return DegreeType.query.filter_by(active=True)
-
-
-def GetActiveDegreeProgrammes():
-
-    return DegreeProgramme.query.filter_by(active=True)
-
-
-def GetActiveSkillGroups():
-
-    return SkillGroup.query.filter_by(active=True)
-
-
-def BuildDegreeProgrammeName(programme):
-
-    return programme.full_name
-
-
-def GetActiveFaculty():
-
-    return db.session.query(User) \
-            .filter(User.active) \
-            .join(FacultyData, FacultyData.id == User.id) \
-            .order_by(User.last_name, User.first_name)
-
-
-def GetPossibleConvenors():
-
-    return db.session.query(FacultyData) \
-            .join(User, User.id == FacultyData.id) \
-            .filter(User.active) \
-            .order_by(User.last_name, User.first_name)
-
-
-def BuildUserRealName(user):
-
-    return user.name_and_username
-
-
-def BuildConvenorRealName(facdata):
-
-    return facdata.user.name_and_username
-
-
-def GetAllProjectClasses():
-
-    return ProjectClass.query.filter_by(active=True)
-
-
-def GetConvenorProjectClasses():
-
-    return ProjectClass.query.filter(ProjectClass.active, ProjectClass.convenor_id==current_user.id)
-
-
-def GetSysadminUsers():
-
-    return User.query.filter(User.active, User.roles.any(Role.name == 'root')).order_by(User.last_name, User.first_name)
+from ..shared.forms.wtf_validators import valid_username, globally_unique_username, unique_or_original_username, \
+    unique_or_original_email, globally_unique_group_abbreviation, unique_or_original_abbreviation, \
+    globally_unique_degree_type, unique_or_original_degree_type, globally_unique_degree_programme, \
+    unique_or_original_degree_programme, globally_unique_transferable_skill, unique_or_original_transferable_skill, \
+    globally_unique_skill_group, unique_or_original_skill_group, globally_unique_project_class, \
+    unique_or_original_project_class, globally_unique_project_class_abbrev, unique_or_original_project_class_abbrev, \
+    globally_unique_supervisor, unique_or_original_supervisor, globally_unique_role, unique_or_original_role, \
+    valid_json, password_strength, OptionalIf, NotOptionalIf
+from ..shared.forms.queries import GetActiveDegreeTypes, GetActiveDegreeProgrammes, GetActiveSkillGroups, \
+    BuildDegreeProgrammeName, GetPossibleConvenors, BuildUserRealName, BuildConvenorRealName, GetAllProjectClasses, \
+    GetConvenorProjectClasses, GetSysadminUsers
+from ..models import BackupConfiguration, EnrollmentRecord, submission_choices, academic_titles, \
+    extent_choices, year_choices
+
+from ..shared.forms.fields import EditFormMixin, CheckboxQuerySelectMultipleField
 
 
 class UniqueUserNameMixin():
@@ -385,6 +102,7 @@ class FacultyDataMixin():
 
     use_academic_title = BooleanField('Use academic title', default=True,
                                       description='Prefix your name with Dr, Professor or similar in student-facing web pages.')
+
     sign_off_students = BooleanField('Ask to confirm student meetings', default=True,
                                      description='If meetings are required before project selection, '
                                                  'confirmation is needed before allowing students to sign up.')
@@ -568,7 +286,8 @@ class ProjectClassMixin():
                                    description='Select number of preferences students should list before joining.')
 
     switch_choices = IntegerField('Number of subsequent project preferences',
-                                  description='Number of preferences to allow in subsequent years, if switching is allowed.')
+                                  description='Number of preferences to allow in subsequent years, '
+                                              'if switching is allowed.')
 
     CATS_supervision = IntegerField('CATS awarded for project supervision',
                                     validators=[DataRequired(message='Please enter an integer value')])
@@ -635,7 +354,7 @@ class AddProjectClassForm(Form, ProjectClassMixin):
 class EditProjectClassForm(Form, ProjectClassMixin, EditFormMixin):
 
     name = StringField('Name', validators=[DataRequired(message='Name of project class is required'),
-                                          unique_or_original_project_class])
+                                           unique_or_original_project_class])
     abbreviation = StringField('Abbreviation', validators=[DataRequired(message='An abbreviation is required'),
                                                            unique_or_original_project_class_abbrev])
 
@@ -680,7 +399,7 @@ class MessageMixin():
 
     dismissible = BooleanField('Allow message to be dismissed')
 
-    title = StringField('Title', validators=[Optional()], description='Optional. Briefly summarize your message.')
+    title = StringField('Title', validators=[Optional()], description='Optional. Summarize your message briefly.')
 
     body = TextAreaField('Message', render_kw={"rows": 5},
                          validators=[DataRequired(message='You must enter a message, however short')])

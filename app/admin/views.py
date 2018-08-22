aa251d9d5829b7f1b600f75b5233665f9f368fc4
ds283 <D.Seery@sussex.ac.uk>
2018-08-22 15:59:07 +0100

    Implement ‘student view’ of matching inspector (MPS-13)

--- app/admin/views.py
+++ app/admin/views.py
@@ -43,7 +43,7 @@ from ..models import db, MainConfig, User, FacultyData, StudentData, ResearchGro
     BackupRecord, TaskRecord, Notification, EnrollmentRecord, Role, MatchingAttempt, MatchingRecord
 
 from ..shared.utils import get_main_config, get_current_year, home_dashboard, get_matching_dashboard_data, \
-    get_root_dashboard_data
+    get_root_dashboard_data, build_match_selector_data
 from ..shared.formatters import format_size
 from ..shared.backup import get_backup_config, set_backup_config, get_backup_count, get_backup_size, remove_backup
 from ..shared.validators import validate_is_convenor
@@ -3090,6 +3090,70 @@ def delete_match(id):
               'error')
 
     return redirect(request.referrer)
+
+
+@admin.route('/match_student_view/<int:id>')
+@roles_required('root')
+def match_student_view(id):
+
+    record = MatchingAttempt.query.get_or_404(id)
+
+    if not record.finished:
+        flash('Match "{name}" is not yet available for inspection '
+              'because the solver has not terminated.'.format(name=record.name), 'error')
+        return redirect(request.referrer)
+
+    if record.outcome != MatchingAttempt.OUTCOME_OPTIMAL:
+        flash('Match "{name}" is not available for inspection '
+              'because it did not yield an optimal solution'.format(name=record.name), 'error')
+        return redirect(request.referrer)
+
+    return render_template('admin/match_inspector/student.html', pane='student', id=id)
+
+
+@admin.route('/match_faculty_view/<int:id>')
+@roles_required('root')
+def match_faculty_view(id):
+
+    record = MatchingAttempt.query.get_or_404(id)
+
+    if not record.finished:
+        flash('Match "{name}" is not yet available for inspection '
+              'because the solver has not terminated.'.format(name=record.name), 'error')
+        return redirect(request.referrer)
+
+    if record.outcome != MatchingAttempt.OUTCOME_OPTIMAL:
+        flash('Match "{name}" is not available for inspection '
+              'because it did not yield an optimal solution'.format(name=record.name), 'error')
+        return redirect(request.referrer)
+
+    return render_template('admin/match_inspector/faculty.html', pane='faculty', id=id)
+
+
+@admin.route('/match_student_view_ajax/<int:id>')
+@roles_required('root')
+def match_student_view_ajax(id):
+
+    record = MatchingAttempt.query.get_or_404(id)
+
+    if not record.finished or record.outcome != MatchingAttempt.OUTCOME_OPTIMAL:
+        return jsonify({})
+
+    data = build_match_selector_data(record)
+
+    return ajax.admin.match_view_student.student_view_data(data)
+
+
+@admin.route('/match_faculty_view_ajax/<int:id>')
+@roles_required('root')
+def match_faculty_view_ajax(id):
+
+    record = MatchingAttempt.query.get_or_404(id)
+
+    if not record.finished or record.outcome != MatchingAttempt.OUTCOME_OPTIMAL:
+        return jsonify({})
+
+    return ajax.admin.match_view_faculty.faculty_view_data([])
 
 
 @admin.route('/terminate_background_task/<string:id>')

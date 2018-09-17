afde6527f9c4b6766d0e718da564d3a1cb6d40b5
ds283 <D.Seery@sussex.ac.uk>
2018-09-17 16:51:12 +0100

    Switch from gunicorn to waitress
    - gunicorn seems to suffer from unexplained CRITICAL_TIMEOUT errors on AWS. There are many reports of this on the internet, but no confirmed fixes

--- boot.sh
+++ boot.sh
@@ -9,4 +9,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn -b :5000 -k gevent --access-logfile - --error-logfile - mpsproject:app
+waitress-serve --port 5000 mpsproject:app

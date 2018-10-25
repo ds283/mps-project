480338413f15274e261f796f814def0dde779557
ds283 <D.Seery@sussex.ac.uk>
2018-10-25 22:05:23 +0100

    Add error & warning list to schedule session view (MPS-67)

--- app/ajax/admin/schedule_view_sessions.py
+++ app/ajax/admin/schedule_view_sessions.py
@@ -11,6 +11,15 @@
 from flask import render_template_string, jsonify
 
 
+_name = \
+"""
+{{ s.session.label|safe }}
+{% if not s.is_valid %}
+    <i class="fa fa-exclamation-triangle" style="color:red;"></i>
+{% endif %}
+"""
+
+
 _assessors = \
 """
 {% for assessor in s.assessors %}
@@ -23,17 +32,60 @@ _assessors = \
 
 _talks = \
 """
+{% set ns = namespace(count=0) %}
 {% for talk in s.talks %}
+    {% set ns.count = ns.count + 1 %}
     <div>
         {% set style = talk.pclass.make_CSS_style() %}
         <span class="label {% if style %}label-default{% else %}label-info{% endif %}" {% if style %}style="{{ style }}"{% endif %}>{{ talk.owner.student.user.name }} &ndash; {{ talk.project.name }} ({{ talk.supervisor.user.name }})</span>
     </div>
 {% endfor %}
+{% if ns.count > 0 %}
+    <p></p>
+    {% set errors = s.errors %}
+    {% set warnings = s.warnings %}
+    {% if errors|length == 1 %}
+        <span class="label label-danger">1 error</span>
+    {% elif errors|length > 1 %}
+        <span class="label label-danger">{{ errors|length }} errors</span>
+    {% else %}
+        <span class="label label-success">0 errors</span>
+    {% endif %}
+    {% if warnings|length == 1 %}
+        <span class="label label-warning">1 warning</span>
+    {% elif warnings|length > 1 %}
+        <span class="label label-warning">{{ warnings|length }} warnings</span>
+    {% else %}
+        <span class="label label-success">0 warnings</span>
+    {% endif %}
+    {% if errors|length > 0 %}
+        <div class="has-error">
+            {% for item in errors %}
+                {% if loop.index <= 10 %}
+                    <p class="help-block">{{ item }}</p>
+                {% elif loop.index == 11 %}
+                    <p class="help-block">...</p>
+                {% endif %}            
+            {% endfor %}
+        </div>
+    {% endif %}
+    {% if warnings|length > 0 %}
+        <div class="has-error">
+            {% for item in warnings %}
+                {% if loop.index <= 10 %}
+                    <p class="help-block">Warning: {{ item }}</p>
+                {% elif loop.index == 11 %}
+                    <p class="help-block">...</p>
+                {% endif %}
+            {% endfor %}
+        </div>
+    {% endif %}
+{% endif %}
 """
 
 
 def schedule_view_sessions(slots, record):
-    data = [{'session': {'display': s.session.label,
+    data = [{'session': {'display': render_template_string(_name, s=s),
                          'sortvalue': s.session.date.isoformat()},
              'room': s.room.label,
              'assessors': render_template_string(_assessors, s=s),

8f3894b3a55d271d7cd0c770e2276d2446fa28e6
ds283 <D.Seery@sussex.ac.uk>
2018-11-04 01:30:04 +0000

    Add Flask HTML minify extension

---
+++ app/minify.py
@@ -0,0 +1,13 @@
+#
+# Created by David Seery on 2018-11-04.
+# Copyright (c) 2018 University of Sussex. All rights reserved.
+#
+# This file is part of the MPS-Project platform developed in
+# the School of Mathematics & Physical Sciences, University of Sussex.
+#
+# Contributors: David Seery <D.Seery@sussex.ac.uk>
+#
+
+from flask_htmlmin import HTMLMIN
+
+htmlmin = HTMLMIN()

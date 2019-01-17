68f035292d8e372af746242ba1187f14d934704f
ds283 <D.Seery@sussex.ac.uk>
2019-01-17 16:10:51 +0000

    Add validation labels to ‘Manage users’ view

--- app/ajax/users/shared.py
+++ app/ajax/users/shared.py
@@ -10,115 +10,127 @@
 
 
 name = \
-    """
-    {{ u.name }}
-    <div>
-        {% if u.theme == u.THEME_DEFAULT %}
-            <span class="label label-primary">Default</span>
-        {% elif u.theme == u.THEME_FLAT %}
-            <span class="label label-primary">Flat</span>
-        {% elif u.theme == u.THEME_DARK %}
-            <span class="label label-primary">Dark</span>
+"""
+{{ u.name }}
+<div>
+    {% if u.student_data and u.student_data is not none %}
+        {% set state = u.student_data.validation_state %}
+        {% if state == u.student_data.VALIDATION_QUEUED %}
+            <span class="label label-warning">NOT VALIDATED</span>
+        {% elif state == u.student_data.VALIDATION_REJECTED %}
+            <span class="label label-danger">REJECTED</span>
+        {% elif state == u.student_data.VALIDATION_VALIDATED %}
+            <span class="label label-success"><i class="fa fa-check"></i></span>
         {% else %}
-            <span class="label label-danger">Unknown theme</span>
+            <span class="label label-danger">Unknown validation</span>
         {% endif %}
-    </div>
-    """
+    {% endif %}
+    {% if u.theme == u.THEME_DEFAULT %}
+        <span class="label label-primary">Default</span>
+    {% elif u.theme == u.THEME_FLAT %}
+        <span class="label label-primary">Flat</span>
+    {% elif u.theme == u.THEME_DARK %}
+        <span class="label label-primary">Dark</span>
+    {% else %}
+        <span class="label label-danger">Unknown theme</span>
+    {% endif %}
+</div>
+"""
 
 
 menu = \
-    """
-    <div class="dropdown">
-        <button class="btn btn-default btn-sm btn-block dropdown-toggle" type="button" data-toggle="dropdown">
-            Actions
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu dropdown-menu-right">
-            <li class="dropdown-header">Edit</li>
+"""
+<div class="dropdown">
+    <button class="btn btn-default btn-sm btn-block dropdown-toggle" type="button" data-toggle="dropdown">
+        Actions
+        <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu dropdown-menu-right">
+        <li class="dropdown-header">Edit</li>
+        <li>
+            <a href="{{ url_for('admin.edit_user', id=user.id, pane=pane) }}">
+                <i class="fa fa-cogs"></i> Account settings...
+            </a>
+        </li>
+        {% if user.has_role('faculty') %}
             <li>
-                <a href="{{ url_for('admin.edit_user', id=user.id, pane=pane) }}">
-                    <i class="fa fa-cogs"></i> Account settings...
+                <a href="{{ url_for('admin.edit_affiliations', id=user.id, pane=pane) }}">
+                    <i class="fa fa-cogs"></i> Affiliations...
                 </a>
             </li>
-            {% if user.has_role('faculty') %}
-                <li>
-                    <a href="{{ url_for('admin.edit_affiliations', id=user.id, pane=pane) }}">
-                        <i class="fa fa-cogs"></i> Affiliations...
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ url_for('admin.edit_enrollments', id=user.id, pane=pane) }}">
-                        <i class="fa fa-cogs"></i> Enrollments...
-                    </a>
-                </li>
-            {% endif %}
-    
-            <li role="separator" class="divider"></li>
-            <li class="dropdown-header">Operations</li>
-    
-            <li {% if user.username == current_user.username or user.has_role('admin') or user.has_role('sysadmin') %}class="disabled"{% endif %}>
-                {% if user.is_active %}
-                    <a {% if user.username != current_user.username or user.has_role('admin') or user.has_role('sysadmin') %}href="{{ url_for('admin.deactivate_user', id=user.id) }}"{% endif %}>
-                        <i class="fa fa-wrench"></i> Make inactive
-                    </a>
-                {% else %}
-                    <a href="{{ url_for('admin.activate_user', id=user.id) }}">
-                        <i class="fa fa-wrench"></i> Make active
-                    </a>
-                {% endif %}
+            <li>
+                <a href="{{ url_for('admin.edit_enrollments', id=user.id, pane=pane) }}">
+                    <i class="fa fa-cogs"></i> Enrollments...
+                </a>
             </li>
-    
-            {# current user always has role of at least 'admin', so no need to check here #}
-            {% if not user.has_role('student') and not user.has_role('root') %}
-                {% if user.has_role('admin') %}
-                    <li {% if user.username == current_user.username %}class="disabled"{% endif %}>
-                        <a {% if user.username != current_user.username %}href="{{ url_for('admin.remove_admin', id=user.id) }}"{% endif %}>
-                            <i class="fa fa-wrench"></i> Remove admin
-                        </a>
-                    </li>
-                {% else %}
-                    <li {% if not user.is_active %}class="disabled"{% endif %}>
-                        <a {% if user.is_active %}href="{{ url_for('admin.make_admin', id=user.id) }}{% endif %}">
-                            <i class="fa fa-wrench"></i> Promote to admin
-                        </a>
-                    </li>
-                {% endif %}
+        {% endif %}
+
+        <li role="separator" class="divider"></li>
+        <li class="dropdown-header">Operations</li>
+
+        <li {% if user.username == current_user.username or user.has_role('admin') or user.has_role('sysadmin') %}class="disabled"{% endif %}>
+            {% if user.is_active %}
+                <a {% if user.username != current_user.username or user.has_role('admin') or user.has_role('sysadmin') %}href="{{ url_for('admin.deactivate_user', id=user.id) }}"{% endif %}>
+                    <i class="fa fa-wrench"></i> Make inactive
+                </a>
+            {% else %}
+                <a href="{{ url_for('admin.activate_user', id=user.id) }}">
+                    <i class="fa fa-wrench"></i> Make active
+                </a>
             {% endif %}
-    
-            {% if current_user.has_role('root') and not user.has_role('student') %}
-                {% if user.has_role('root') %}
-                    <li {% if user.username == current_user.username %}class="disabled"{% endif %}>
-                        <a {% if user.username != current_user.username %}href="{{ url_for('admin.remove_root', id=user.id) }}"{% endif %}>
-                            <i class="fa fa-wrench"></i> Remove sysadmin
-                        </a>
-                    </li>
-                {% else %}
-                    <li {% if not user.is_active %}class="disabled"{% endif %}>
-                        <a {% if user.is_active %}href="{{ url_for('admin.make_root', id=user.id) }}{% endif %}">
-                            <i class="fa fa-wrench"></i> Promote to sysadmin
-                        </a>
-                    </li>
-                {% endif %}
-            {% endif %}
-    
-            {# check whether we should offer role editor in the menu #}
-            {% if current_user.has_role('root') and not user.has_role('student') %}
-                <li>
-                    <a href="{{ url_for('admin.assign_roles', id=user.id) }}">
-                        <i class="fa fa-wrench"></i> Assign roles...
+        </li>
+
+        {# current user always has role of at least 'admin', so no need to check here #}
+        {% if not user.has_role('student') and not user.has_role('root') %}
+            {% if user.has_role('admin') %}
+                <li {% if user.username == current_user.username %}class="disabled"{% endif %}>
+                    <a {% if user.username != current_user.username %}href="{{ url_for('admin.remove_admin', id=user.id) }}"{% endif %}>
+                        <i class="fa fa-wrench"></i> Remove admin
+                    </a>
+                </li>
+            {% else %}
+                <li {% if not user.is_active %}class="disabled"{% endif %}>
+                    <a {% if user.is_active %}href="{{ url_for('admin.make_admin', id=user.id) }}{% endif %}">
+                        <i class="fa fa-wrench"></i> Promote to admin
                     </a>
                 </li>
             {% endif %}
-            
-            {% if current_user.has_role('root') and not current_user.has_role('student') %}
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Superuser functions</li>
-                <li>
-                    <a href="{{ url_for('admin.login_as', id=user.id) }}">
-                        <i class="fa fa-user"></i> Login as user
+        {% endif %}
+
+        {% if current_user.has_role('root') and not user.has_role('student') %}
+            {% if user.has_role('root') %}
+                <li {% if user.username == current_user.username %}class="disabled"{% endif %}>
+                    <a {% if user.username != current_user.username %}href="{{ url_for('admin.remove_root', id=user.id) }}"{% endif %}>
+                        <i class="fa fa-wrench"></i> Remove sysadmin
+                    </a>
+                </li>
+            {% else %}
+                <li {% if not user.is_active %}class="disabled"{% endif %}>
+                    <a {% if user.is_active %}href="{{ url_for('admin.make_root', id=user.id) }}{% endif %}">
+                        <i class="fa fa-wrench"></i> Promote to sysadmin
                     </a>
                 </li>
             {% endif %}
-        </ul>
-    </div>
-    """
+        {% endif %}
+
+        {# check whether we should offer role editor in the menu #}
+        {% if current_user.has_role('root') and not user.has_role('student') %}
+            <li>
+                <a href="{{ url_for('admin.assign_roles', id=user.id) }}">
+                    <i class="fa fa-wrench"></i> Assign roles...
+                </a>
+            </li>
+        {% endif %}
+        
+        {% if current_user.has_role('root') and not current_user.has_role('student') %}
+            <li role="separator" class="divider"></li>
+            <li class="dropdown-header">Superuser functions</li>
+            <li>
+                <a href="{{ url_for('admin.login_as', id=user.id) }}">
+                    <i class="fa fa-user"></i> Login as user
+                </a>
+            </li>
+        {% endif %}
+    </ul>
+</div>
+"""

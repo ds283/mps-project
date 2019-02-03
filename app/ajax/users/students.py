505007889b6e9fbb73065f73b431233a5f07ebc6
ds283 <D.Seery@sussex.ac.uk>
2019-02-03 08:41:01 +0000

    Improve performance of manage_users view by caching

--- app/ajax/users/students.py
+++ app/ajax/users/students.py
@@ -11,14 +11,23 @@
 from ...shared.utils import get_current_year
 from flask import render_template_string, jsonify
 
+from ...database import db
+from ...models import User, StudentData
+from ...cache import cache
+
+from sqlalchemy.event import listens_for
+
 from .shared import menu, name
 
 
-def build_student_data(students):
+@cache.memoize()
+def _element(user_id):
+    u = db.session.query(User).filter_by(id=user_id).one()
+    s = db.session.query(StudentData).filter_by(id=user_id).one()
 
     year = get_current_year()
 
-    data = [{'name': {
+    return {'name': {
                 'display': render_template_string(name, u=u),
                 'sortstring': u.last_name + u.first_name},
              'active': u.active_label,
@@ -27,6 +36,46 @@ def build_student_data(students):
              'acadyear': {
                  'display': s.academic_year_label(year),
                  'sortvalue': s.academic_year(year)},
-             'menu': render_template_string(menu, user=u, pane='students')} for s, u in students]
+             'menu': render_template_string(menu, user=u, pane='students')}
+
+
+@listens_for(User, 'before_insert')
+def _User_insert_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        cache.delete_memoized(_element, target.id)
+
+
+@listens_for(User, 'before_update')
+def _User_update_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        cache.delete_memoized(_element, target.id)
+
+
+@listens_for(User, 'before_delete')
+def _User_delete_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        cache.delete_memoized(_element, target.id)
+
+
+@listens_for(StudentData, 'before_insert')
+def _StudentData_insert_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        cache.delete_memoized(_element, target.id)
+
+
+@listens_for(StudentData, 'before_update')
+def _StudentData_update_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        cache.delete_memoized(_element, target.id)
+
+
+@listens_for(StudentData, 'before_delete')
+def _StudentData_delete_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        cache.delete_memoized(_element, target.id)
+
+
+def build_student_data(student_ids):
+    data = [_element(s_id) for s_id in student_ids]
 
     return jsonify(data)

3e25dda691758f26fcaab951061fc2111c63e804
ds283 <D.Seery@sussex.ac.uk>
2019-02-03 02:31:12 +0000

    Dramatically improve speed of approvals view by using caching

--- app/ajax/user_approver/correct.py
+++ app/ajax/user_approver/correct.py
@@ -8,9 +8,17 @@
 # Contributors: David Seery <D.Seery@sussex.ac.uk>
 #
 
-from flask import render_template_string, jsonify, url_for
+from flask import render_template_string, jsonify, current_app
+
+from ...database import db
+from ...models import StudentData
+from ...cache import cache
+
+from sqlalchemy.event import listens_for
 
 from ...shared.utils import get_current_year
+
+from urllib import parse
 
 
 _actions = \
@@ -31,16 +39,58 @@ _rejected = \
 """
 
 
-def correction_data(records, url=None, text=None):
-    current_year = get_current_year()
+@cache.memoize()
+def _element(record_id, current_year):
+    r = db.session.query(StudentData).filter_by(id=record_id).one()
 
-    data = [{'name': {'display': r.user.name,
+    return {'name': {'display': r.user.name,
                       'sortstring': r.user.last_name + r.user.first_name},
              'email': r.user.email,
              'exam_number': r.exam_number,
              'programme': r.programme.full_name,
              'year': r.academic_year_label(current_year),
              'rejected_by': render_template_string(_rejected, s=r),
-             'menu': render_template_string(_actions, s=r, url=url, text=text)} for r in records]
+             'menu': render_template_string(_actions, s=r, url='REPURL', text='REPTEXT')}
+
+
+@listens_for(StudentData, 'before_insert')
+def _StudentData_insert_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        current_year = get_current_year()
+        cache.delete_memoized(_element, target.id, current_year)
+
+
+@listens_for(StudentData, 'before_update')
+def _StudentData_update_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        current_year = get_current_year()
+        cache.delete_memoized(_element, target.id, current_year)
+
+
+@listens_for(StudentData, 'before_delete')
+def _StudentData_delete_handler(mapper, connection, target):
+    with db.session.no_autoflush:
+        current_year = get_current_year()
+        cache.delete_memoized(_element, target.id, current_year)
+
+
+def correction_data(records, url=None, text=None):
+    current_year = get_current_year()
+
+    bleach = current_app.extensions['bleach']
+
+    def urlencode(s):
+        s = s.encode('utf8')
+        s = parse.quote_plus(s)
+        return bleach.clean(s)
+
+    url_enc = urlencode(url) if url is not None else None
+    text_enc = urlencode(text) if text is not None else None
+
+    def update(d):
+        d.update({'menu': d['menu'].replace('REPURL', url_enc, 1).replace('REPTEXT', text_enc, 1)})
+        return d
+
+    data = [update(_element(r.id, current_year)) for r in records]
 
     return jsonify(data)

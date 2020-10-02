6bffae9c4471699f5ef3dea6363076075caf38d3
ds283 <D.Seery@sussex.ac.uk>
2020-10-02 09:50:37 +0100

    Performance improvements to student user view menu generation
    - also add “Show history…” option

--- app/ajax/users/shared.py
+++ app/ajax/users/shared.py
@@ -57,6 +57,12 @@ name = \
 
 menu = \
 """
+{% set user_is_student = user.has_role('student') %}
+{% set user_is_faculty = user.has_role('faculty') %}
+{% set user_is_admin   = user.has_role('admin') %}
+{% set user_is_root    = user.has_role('root') %}
+{% set cuser_is_admin  = cuser.has_role('admin') %}
+{% set cuser_is_root   = cuser.has_role('root') %}
 <div class="dropdown">
     <button class="btn btn-secondary btn-sm btn-block dropdown-toggle" type="button" data-toggle="dropdown">
         Actions
@@ -66,19 +72,23 @@ menu = \
         <a class="dropdown-item" href="{{ url_for('manage_users.edit_user', id=user.id, pane=pane) }}">
             <i class="fas fa-cogs fa-fw"></i> Account settings...
         </a>
-        {% if user.has_role('faculty') %}
+        {% if user_is_faculty %}
             <a class="dropdown-item" href="{{ url_for('manage_users.edit_affiliations', id=user.id, pane=pane) }}">
                 <i class="fas fa-cogs fa-fw"></i> Affiliations...
             </a>
             <a class="dropdown-item" href="{{ url_for('manage_users.edit_enrollments', id=user.id, pane=pane) }}">
                 <i class="fas fa-cogs fa-fw"></i> Enrollments...
             </a>
+        {% elif user_is_student %}
+            <a class="dropdown-item" href="{{ url_for('student.timeline', student_id=user.id, url=url_for('manage_users.edit_users_students'), text='student accounts') }}">
+                <i class="fas fa-history fa-fw"></i> Show history...
+            </a>
         {% endif %}
 
         <div role="separator" class="dropdown-divider"></div>
         <div class="dropdown-header">Operations</div>
 
-        {% set disabled = (user.username == cuser.username or user.has_role('admin') or user.has_role('sysadmin')) %}
+        {% set disabled = (user.username == cuser.username or user_is_admin or user_is_root) %}
         {% if user.is_active %}
             <a class="dropdown-item {% if disabled %}disabled{% endif %}" {% if user.username != cuser.username or user.has_role('admin') or user.has_role('sysadmin') %}href="{{ url_for('manage_users.deactivate_user', id=user.id) }}"{% endif %}>
                 <i class="fas fa-wrench fa-fw"></i> Make inactive
@@ -90,8 +100,8 @@ menu = \
         {% endif %}
 
         {# current user always has role of at least 'admin', so no need to check here #}
-        {% if not user.has_role('student') and not user.has_role('root') %}
-            {% if user.has_role('admin') %}
+        {% if not user_is_student and not user_is_root %}
+            {% if user_is_admin %}
                 {% set disabled = (user.username == cuser.username) %}
                 <a class="dropdown-item {% if disabled %}disabled{% endif %}" {% if user.username != cuser.username %}href="{{ url_for('manage_users.remove_admin', id=user.id) }}"{% endif %}>
                     <i class="fas fa-wrench fa-fw"></i> Remove admin
@@ -104,8 +114,8 @@ menu = \
             {% endif %}
         {% endif %}
 
-        {% if cuser.has_role('root') and not user.has_role('student') %}
-            {% if user.has_role('root') %}
+        {% if cuser_is_root and not user_is_student %}
+            {% if user_is_root %}
                 {% set disabled = (user.username == cuser.username) %}
                 <a class="dropdown-item {% if disabled %}disabled{% endif %}" {% if user.username != cuser.username %}href="{{ url_for('manage_users.remove_root', id=user.id) }}"{% endif %}>
                     <i class="fas fa-wrench fa-fw"></i> Remove sysadmin
@@ -119,13 +129,13 @@ menu = \
         {% endif %}
 
         {# check whether we should offer role editor in the menu #}
-        {% if cuser.has_role('root') and not user.has_role('student') %}
+        {% if cuser_is_root and not user_is_student %}
             <a class="dropdown-item" href="{{ url_for('manage_users.assign_roles', id=user.id, pane=pane) }}">
                 <i class="fas fa-wrench fa-fw"></i> Assign roles...
             </a>
         {% endif %}
         
-        {% if cuser.has_role('root') and not cuser.has_role('student') %}
+        {% if cuser_is_rot and not cuser_is_strudent %}
             <div role="separator" class="dropdown-divider"></div>
             <div class="dropdown-header">Superuser functions</div>
             <a class="dropdown-item" href="{{ url_for('admin.login_as', id=user.id) }}">

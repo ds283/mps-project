68a0b4fbc6c3e24957c8afbf0864b920c3eb1591
ds283 <D.Seery@sussex.ac.uk>
2022-03-15 23:23:40 +0000

    Upgrade to SQLAlchemy 1.4
    - requires changes to get_count() method, cf. https://gist.github.com/hest/8798884

--- app/shared/sqlalchemy.py
+++ app/shared/sqlalchemy.py
@@ -14,42 +14,10 @@ from sqlalchemy.orm import lazyload
 
 # taken from https://gist.github.com/hest/8798884
 def get_count(q):
-    disable_group_by = False
+    count_q = q.statement.with_only_columns([func.count()]).order_by(None)
+    count = q.session.execute(count_q).scalar()
 
-    if len(q._entities) > 1:
-        # currently support only one entity
-        raise Exception('only one entity is supported for get_count, got: %s' % q)
-
-    entity = q._entities[0]
-
-    if hasattr(entity, 'column'):
-        # _ColumnEntity has column attr - on case: query(Model.column)...
-        col = entity.column
-        if q._group_by and q._distinct:
-            # which query can have both?
-            raise NotImplementedError
-        if q._group_by or q._distinct:
-            col = distinct(col)
-        if q._group_by:
-            # need to disable group_by and enable distinct - we can do this because we have only 1 entity
-            disable_group_by = True
-        count_func = func.count(col)
-
-    else:
-        # _MapperEntity doesn't have column attr - on case: query(Model)...
-        count_func = func.count()
-
-    if q._group_by and not disable_group_by:
-        count_func = count_func.over(None)
-
-    count_q = q.options(lazyload('*')).statement.with_only_columns([count_func]).order_by(None)
-
-    if disable_group_by:
-        count_q = count_q.group_by(None)
-
-    value = q.session.execute(count_q).scalar()
-
-    return value if value is not None else 0
+    return count if count is not None else 0
 
 
 # taken from https://stackoverflow.com/questions/28871406/how-to-clone-a-sqlalchemy-db-object-with-new-primary-key

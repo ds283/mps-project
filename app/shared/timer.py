a24facb75e4a254cac8dffe161ace9a1eb31c179
ds283 <D.Seery@sussex.ac.uk>
2022-03-24 21:42:24 +0000

    Remove deprecated time.clock() in favour in time.perf_counter()

--- app/shared/timer.py
+++ app/shared/timer.py
@@ -13,9 +13,9 @@ import time
 
 class Timer:
     def __enter__(self):
-        self.start = time.clock()
+        self.start = time.perf_counter()
         return self
 
     def __exit__(self, *args):
-        self.end = time.clock()
+        self.end = time.perf_counter()
         self.interval = self.end - self.start

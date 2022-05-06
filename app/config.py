13b9ce174bad58c139b162108ac129a895eccd47
ds283 <D.Seery@sussex.ac.uk>
2022-05-06 10:00:46 +0100

    Bump version to 2022.2

--- app/config.py
+++ app/config.py
@@ -27,7 +27,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 # website revision number
-site_revision = '2022.1'
+site_revision = '2022.2'
 
 # website copyright dates
 site_copyright_dates = '2018–2022'

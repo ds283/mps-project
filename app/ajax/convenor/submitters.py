c5b64dc669513216e910ab79cf7c843627b45860
ds283 <D.Seery@sussex.ac.uk>
2022-05-10 00:55:40 +0100

    Tweak minor formatting issue

--- app/ajax/convenor/submitters.py
+++ app/ajax/convenor/submitters.py
@@ -310,7 +310,7 @@ _menu = \
         <a class="dropdown-item d-flex gap-2 {% if disabled %}disabled{% endif %}" {% if not disabled %}href="{{ url_for('documents.submitter_documents', sub_id=sub.id, text='submitters view', url=url_for('convenor.submitters', id=pclass.id)) }}"{% endif %}>
             <i class="fas fa-file fa-fw"></i> Manage documents...
         </a>
-        {% if record.report_id is none and  period.canvas_enabled and not period.closed and record.canvas_submission_available is true %}
+        {% if record.report_id is none and period.canvas_enabled and not period.closed and record.canvas_submission_available is true %}
             <a class="dropdown-item d-flex gap-2" href="{{ url_for('documents.pull_report_from_canvas', rid=record.id, url=url_for('convenor.submitters', id=pclass.id)) }}">
                 <i class="fas fa-download fa-fw"></i> Pull report
             </a>

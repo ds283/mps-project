ad7f8cffeee2781c350c30dd76cd1d04d1c79665
ds283 <D.Seery@sussex.ac.uk>
2022-12-09 01:46:16 +0000

    Minor tweak

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -394,14 +394,14 @@ class ProjectClassMixin():
                                       "student level")
 
     advertise_research_group = BooleanField('Advertise research group affiliations to students',
-                                            description='If selected, students will be shown the research group '
+                                            description='Students will be shown the research group '
                                                         'associated with each project. For example, this could be '
                                                         'used to drive improved awareness of research teams '
                                                         'within the department.')
 
     use_project_tags = BooleanField('Use tags',
-                                    description='If selected, each project variant can be given one or more '
-                                                'tags. The tags are advertised to students.')
+                                    description='Each project variant can be given one or more '
+                                                'tags, which are advertised to students.')
 
 
 class AddProjectClassForm(Form, ProjectClassMixin):

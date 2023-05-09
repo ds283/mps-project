e60cfdce9efa71465983c51da12dca52f0ef2bf2
ds283 <D.Seery@sussex.ac.uk>
2023-05-09 13:37:08 +0100

    Refactor remainder of workload report

--- app/ajax/reports/workload.py
+++ app/ajax/reports/workload.py
@@ -43,6 +43,11 @@ _groups = \
 # language=jinja2
 _full_enrollments = \
 """
+{%- macro projects_list(projects) -%}
+    {%- for p in projects -%}
+        <div>{{ loop.index }}. {{ p.name }}</div>
+    {%- endfor -%}    
+{%- endmacro -%}
 {% for record in enrolments %}
     {% set config = configs[record.pclass_id] %}
     <div class="bg-light p-2 mb-2">
@@ -71,7 +76,7 @@ _full_enrollments = \
                                 {% else %}
                                     {{ offered }} projects
                                 {% endif %}
-                                <i class="fas fa-info-circle" tabindex="0" data-bs-toggle="popover" title="Projects offered" data-bs-container="body" data-bs-trigger="focus" data-bs-content="{% for p in projects %}<div>{{ loop.index }}. {{ p.name }}</div>{% endfor %}"></i>
+                                <i class="fas fa-info-circle" tabindex="0" data-bs-toggle="popover" title="Projects offered" data-bs-container="body" data-bs-trigger="focus" data-bs-content="{{ projects_list(projects) }}"></i>
                             </span>
                         {% endif %}
                     {% elif record.supervisor_state == record.SUPERVISOR_SABBATICAL %}
@@ -179,22 +184,44 @@ _full_enrollments = \
 # language=jinja2
 _simple_enrollments = \
 """
+{%- macro projects_list(projects) -%}
+    {%- for p in projects -%}
+        <div>{{ loop.index }}. {{ p.name }}</div>
+    {%- endfor -%}    
+{%- endmacro -%}
 {% for record in enrolments %}
     {% set config = configs[record.pclass_id] %}
-    <div {% if loop.index > 1 %}style="top-padding: 8px;"{% endif %}>
-        {{ record.pclass.make_label()|safe }}
-
-        {% if config.uses_supervisor %}
-            {% set offered = f.number_projects_offered(record.pclass) %}
-            {% if offered > 0 %}
-                <span class="badge bg-info text-dark" data-bs-toggle="tooltip" data-html="true" title="{% for p in projects %}<p>{{ loop.index }}. {{ p.name }}</p>{% endfor %}">Offered={{ offered }}</span>
-            {% else %}
-                <span class="badge bg-danger">Offered=0</span>
+    <div class="bg-light p-1 mb-1">
+        <div class="d-flex flex-row justify-content-start align-items-center gap-1">
+            {% set swatch_colour = record.pclass.make_CSS_style() %}
+            {% if swatch_colour is not none %}
+                <div class="me-1" style="width: 1rem; height: 1rem; {{ swatch_colour|safe }}"></div>
             {% endif %}
-        {% endif %}
+            {{ record.pclass.name }}
+            {% if config.uses_supervisor %}
+                {% if record.supervisor_state == record.SUPERVISOR_ENROLLED %}
+                    {% set offered = f.number_projects_offered(record.pclass) %}
+                    <div>
+                        {% if offered == 0 %}
+                            <span class="ms-1 small text-danger">No projects</span>
+                        {% else %}
+                            {% set projects = f.projects_offered(record.pclass) %}
+                            <span class="ms-1 small text-muted">
+                                {% if offered == 1 %}
+                                    1 project
+                                {% else %}
+                                    {{ offered }} projects
+                                {% endif %}
+                                <i class="fas fa-info-circle" tabindex="0" data-bs-toggle="popover" title="Projects offered" data-bs-container="body" data-bs-trigger="focus" data-bs-content="{{ projects_list(projects) }}"></i>
+                            </span>
+                        {% endif %}
+                    </div>
+                {% endif %}
+            {% endif %}
+        </div>
     </div>
 {% else %}
-    <span class="badge bg-secondary">None</span>
+    <span class="alert alert-danger p-1"><strong>None</strong></span>
 {% endfor %}
 """
 
@@ -202,20 +229,32 @@ _simple_enrollments = \
 # language=jinja2
 _full_workload = \
 """
-{% for record in f.ordered_enrollments %}
-    {{ record.pclass.make_label(record.pclass.abbreviation + ' ' + wkld[record.pclass_id]|string)|safe }}
-{% else %}
-    <span class="badge bg-secondary">None</span>
+{% set ns = namespace(count=0) %}
+{% for record in enrolments %}
+    {% if record.pclass_id in wkld %}
+        {% set CATS = wkld[record.pclass_id] %}
+        {% if CATS > 0 %}
+            {% set ns.count = ns.count+1 %}
+            <div class="d-flex flex-row justify-content-start align-items-center gap-1">
+                {% set swatch_colour = record.pclass.make_CSS_style() %}
+                {% if swatch_colour is not none %}
+                    <div class="me-1" style="width: 0.8rem; height: 0.8rem; {{ swatch_colour|safe }}"></div>
+                {% endif %}
+                <span class="small">{{ record.pclass.abbreviation }}</span>
+                <span class="text-success small">{{ CATS }} CATS</span>
+            </div>
+        {% endif %}
+    {% endif %}
 {% endfor %}
-<p></p>
-<span class="badge bg-primary">Total {{ tot }}</span>
+{% if ns.count > 0 %}<hr>{% endif %}
+<span class="text-primary mt-2"><strong>{{ tot }} CATS</strong></span>
 """
 
 
 # language=jinja2
 _simple_workload = \
 """
-<span class="badge bg-primary">{{ tot }}</span>
+<span class="text-primary"><strong>{{ tot }} CATS</strong></span>
 """
 
 
@@ -223,40 +262,163 @@ _simple_workload = \
 _availability = \
 """
 {% if u %}
-    <span class="badge bg-info text-dark" data-bs-toggle="tooltip" title="One or more projects do not have a limit on the number of students">Unbounded</span>
+    <span class="text-danger">Unbounded</span>
+    <i class="text-muted fas fa-info-circle" data-bs-toggle="tooltip" title="Unbounded availability" data-bs-html="true" title="<em>Unlimited</em> availability means that one or more projects do not have a limit on the number of students"></i>
 {% else %}
-    <span data-bs-toggle="tooltip" data-html="true" title="<i>Availability</i> is the maximum CATS-weighted number of students who could be assigned to this supervisor">{{ t|round(2) }}</span>
+    <span class="text-success">{{ t|round(2) }}</span>
 {% endif %}
 """
 
 
 # language=jinja2
-_full_assignments = \
+_full_allocation = \
 """
-{% for record in f.ordered_enrollments %}
-    {{ record.pclass.make_label(record.pclass.abbreviation + ' ' + data[record.pclass_id]|string)|safe }}
-{% else %}
-    <span class="badge bg-secondary">None</span>
-{% endfor %}
-<p></p>
-<span class="badge bg-primary">Total {{ total }}</span>
+{%- macro truncate_name(name, maxlength=25) -%}
+    {%- if name|length > maxlength -%}
+        {{ name[0:maxlength] }}...
+    {%- else -%}
+        {{ name }}
+    {%- endif -%}
+{%- endmacro -%}
+{# notice need to use single quotes everywhere if the definition of this macro, because it has to go in a data-bs-content attribute #}
+{%- macro assigned_list(assigned) -%}
+    {%- for record in assigned -%}
+        <div class='small'>
+            {{ loop.index }}. {{ record.owner.student.user.name }}
+            {% if record.project is not none -%}
+                <em>{{ truncate_name(record.project.name, maxlength=40) }}</em>
+            {%- endif -%}
+        </div>
+    {%- else -%}
+        <span class='small text-danger'><strong>None</strong></span>
+    {%- endfor -%}
+{%- endmacro -%}
+{% macro allocation_list(enrolments, num_dict, CATS_dict, assigned_dict, total, tabindex, title) %}
+    {% for record in enrolments %}
+        {% if record.pclass_id in num_dict %}
+            {% set num = num_dict[record.pclass_id] %}
+            {% set CATS = CATS_dict[record.pclass_id] %}
+            {% set assigned = assigned_dict[record.pclass_id] %}
+            {% if num > 0 %}
+                <div class="d-flex flex-row justify-content-start align-items-center gap-2">
+                    {% set swatch_colour = record.pclass.make_CSS_style() %}
+                    {% if swatch_colour is not none %}
+                        <div class="me-1" style="width: 1rem; height: 1rem; {{ swatch_colour|safe }}"></div>
+                    {% endif %}
+                    <span class="small">{{ record.pclass.abbreviation }}</span>
+                    <span class="small text-success"><strong>{{ num }}</strong> &rightarrow; {{ CATS }} CATS</span>
+                    <i class="small text-muted fas fa-info-circle" tabindex="{{ tabindex }}" data-bs-toggle="popover" title="{{ title }}" data-bs-container="body" data-bs-trigger="focus" data-bs-content="{{ assigned_list(assigned)|safe }}"></i>
+                </div>
+            {% endif %}
+        {% endif %}
+    {% endfor %}
+    <div class="mt-1 small text-secondary">
+        {{ total }} allocations &rightarrow; {{ CATS_dict.values()|sum }} CATS
+    </div>
+{% endmacro %}
+{% macro presentation_list(enrolments, num_dict, CATS_dict, assigned_dict, total, tabindex, title) %}
+    {% for record in enrolments %}
+        {% if record.pclass_id in num_dict %}
+            {% set num = num_dict[record.pclass_id] %}
+            {% set CATS = CATS_dict[record.pclass_id] %}
+            {% if num > 0 %}
+                <div class="d-flex flex-row justify-content-start align-items-center gap-2">
+                    {% set swatch_colour = record.pclass.make_CSS_style() %}
+                    {% if swatch_colour is not none %}
+                        <div class="me-1" style="width: 1rem; height: 1rem; {{ swatch_colour|safe }}"></div>
+                    {% endif %}
+                    <span class="small">{{ record.pclass.abbreviation }}</span>
+                    <span class="small text-success"><strong>{{ num }}</strong> &rightarrow; {{ CATS }} CATS</span>
+                </div>
+            {% endif %}
+        {% endif %}
+    {% endfor %}
+    <div class="mt-1 small text-secondary">
+        {{ total }} allocations &rightarrow; {{ CATS_dict.values()|sum }} CATS
+    </div>
+{% endmacro %}
+{% if total_supervising > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Supervising</span>
+        {{ allocation_list(enrolments, num_supervising, CATS_supervising, assigned_supervising, total_supervising, "10", "Supervision allocation") }}
+    </div>
+{% endif %}
+{% if total_marking > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Marking</span>
+        {{ allocation_list(enrolments, num_marking, CATS_marking, assigned_marking, total_marking, "11", "Marking allocation") }}
+    </div>
+{% endif %}
+{% if total_moderating > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Moderating</span>
+        {{ allocation_list(enrolments, num_moderating, CATS_moderating, assigned_moderating, total_moderating, "12", "Moderating allocation") }}
+    </div>
+{% endif %}
+{% if total_presentations > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Presentations</span>
+        {{ presentation_list(enrolments, num_presentations, CATS_presentations, assigned_presentations, total_presentations, "13", "Presentations allocation") }}
+    </div>
+{% endif %}
 """
 
 
 # language=jinja2
-_simple_assignments = \
+_simple_allocation = \
 """
-<span class="badge bg-primary">Total {{ total }}</span>
+{% macro allocation_list(CATS_dict, total) %}
+    <div class="mt-1 small text-secondary">
+        {{ total }} allocations &rightarrow; {{ CATS_dict.values()|sum }} CATS
+    </div>
+{% endmacro %}
+{% if total_supervising > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Supervising</span>
+        {{ allocation_list(CATS_supervising, total_supervising) }}
+    </div>
+{% endif %}
+{% if total_marking > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Marking</span>
+        {{ allocation_list(CATS_marking, total_marking) }}
+    </div>
+{% endif %}
+{% if total_moderating > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Moderating</span>
+        {{ allocation_list(CATS_moderating, total_moderating) }}
+    </div>
+{% endif %}
+{% if total_presentations > 0 %}
+    <div class="bg-light p-2 mb-2">
+        <span>Presentations</span>
+        {{ allocation_list(CATS_presentations, total_presentations) }}
+    </div>
+{% endif %}
 """
 
 
-def _element_base(faculty_id, enrolment_template, assignments_template, workload_template):
+def _element_base(faculty_id, enrolment_template, allocation_template, workload_template):
     f: FacultyData = db.session.query(FacultyData).filter_by(id=faculty_id).one()
 
-    workload = {}
-    supervising = {}
-    marking = {}
-    presentations = {}
+    CATS_workload = {}
+
+    CATS_supervising = {}
+    CATS_marking = {}
+    CATS_moderating = {}
+    CATS_presentations = {}
+
+    num_supervising = {}
+    num_marking = {}
+    num_moderating = {}
+    num_presentations = {}
+
+    assigned_supervising = {}
+    assigned_marking = {}
+    assigned_moderating = {}
+    assigned_presentations = {}
+
     configs = {}
 
     total_workload = 0
@@ -266,29 +428,53 @@ def _element_base(faculty_id, enrolment_template, assignments_template, workload
     for record in enrolments:
         record: EnrollmentRecord
 
-        CATS = sum(f.CATS_assignment(record.pclass))
+        supv, mark, mod, pres = f.CATS_assignment(record.pclass)
+
         pclass_id = record.pclass_id
-        workload[pclass_id] = CATS
+
+        CATS_total = supv + mark + mod + pres
+        CATS_workload[pclass_id] = CATS_total
+        total_workload += CATS_total
+
+        CATS_supervising[pclass_id] = supv
+        CATS_marking[pclass_id] = mark
+        CATS_moderating[pclass_id] = mod
+        CATS_presentations[pclass_id] = pres
 
         config: ProjectClassConfig = record.pclass.most_recent_config
         configs[pclass_id] = config
 
         if config.uses_supervisor:
-            supervising[pclass_id] = get_count(f.supervisor_assignments(config_id=config.id))
+            data = f.supervisor_assignments(config_id=config.id)
+            assigned_supervising[pclass_id] = data.all()
+            num_supervising[pclass_id] = get_count(data)
 
         if config.uses_marker:
-            marking[pclass_id] = get_count(f.marker_assignments(config_id=config.id))
+            data = f.marker_assignments(config_id=config.id)
+            assigned_marking[pclass_id] = data.all()
+            num_marking[pclass_id] = get_count(data)
+
+        if config.uses_moderator:
+            data = f.moderator_assignments(config_id=config.id)
+            assigned_moderating[pclass_id] = data.all()
+            num_moderating[pclass_id] = get_count(data)
 
         if config.uses_presentations:
-            presentations[pclass_id] = get_count(f.presentation_assignments(config_id=config.id))
+            data = f.presentation_assignments(config_id=config.id)
+            assigned_presentations[pclass_id] = data.all()
+            num_presentations[pclass_id] = get_count(data)
 
-        total_workload += CATS
+    total_supervising = sum(num_supervising.values())
+    total_marking = sum(num_marking.values())
+    total_moderating = sum(num_moderating.values())
+    total_presentations = sum(num_presentations.values())
 
-    total_supervising = sum(supervising.values())
-    total_marking = sum(marking.values())
-    total_presentations = sum(presentations.values())
+    total_allocation = total_supervising + total_marking + total_moderating + total_presentations
 
-    total, unbounded = f.student_availability
+    availability, unbounded = f.student_availability
+
+    # TODO: presentation ScheduleSlot allocation isn't currently attached to the "allocation" entry -
+    #  these should be shown, just as for supervising/marking/moderating allocation
 
     return {'name': {'display': render_template_string(_name, f=f),
                      'sortstring': f.user.last_name + f.user.first_name},
@@ -296,26 +482,39 @@ def _element_base(faculty_id, enrolment_template, assignments_template, workload
             'enrollments': {'display': render_template_string(enrolment_template, f=f, enrolments=enrolments,
                                                               configs=configs),
                             'sortvalue': get_count(f.enrollments)},
-            'supervising': {'display': render_template_string(assignments_template, f=f, data=supervising, total=total_supervising),
-                            'sortvalue': total_supervising},
-            'marking': {'display': render_template_string(assignments_template, f=f, data=marking, total=total_marking),
-                        'sortvalue': total_marking},
-            'presentations': {'display': render_template_string(assignments_template, f=f, data=presentations, total=total_presentations),
-                              'sortvalue': total_presentations},
-            'availability': {'display': render_template_string(_availability, t=total, u=unbounded),
-                             'sortvalue': 999999 if unbounded else total},
-            'workload': {'display': render_template_string(workload_template, f=f, wkld=workload, tot=total_workload),
+            'allocation': {'display': render_template_string(allocation_template, f=f, enrolments=enrolments,
+                                                             CATS_supervising=CATS_supervising,
+                                                             num_supervising=num_supervising,
+                                                             assigned_supervising=assigned_supervising,
+                                                             CATS_marking=CATS_marking,
+                                                             num_marking=num_marking,
+                                                             assigned_marking=assigned_marking,
+                                                             CATS_moderating=CATS_moderating,
+                                                             num_moderating=num_moderating,
+                                                             assigned_moderating=assigned_moderating,
+                                                             CATS_presentations=CATS_presentations,
+                                                             num_presentations=num_presentations,
+                                                             assigned_presentations=assigned_presentations,
+                                                             total_supervising=total_supervising,
+                                                             total_marking=total_marking,
+                                                             total_moderating=total_moderating,
+                                                             total_presentations=total_presentations),
+                           'sortvalue': total_allocation},
+            'availability': {'display': render_template_string(_availability, t=availability, u=unbounded),
+                             'sortvalue': 999999 if unbounded else availability},
+            'workload': {'display': render_template_string(workload_template, f=f, enrolments=enrolments,
+                                                           wkld=CATS_workload, tot=total_workload),
                          'sortvalue': total_workload}}
 
 
 @cache.memoize()
 def _element_full(faculty_id):
-    return _element_base(faculty_id, _full_enrollments, _full_assignments, _full_workload)
+    return _element_base(faculty_id, _full_enrollments, _full_allocation, _full_workload)
 
 
 @cache.memoize()
 def _element_simple(faculty_id):
-    return _element_base(faculty_id, _simple_enrollments, _simple_assignments, _simple_workload)
+    return _element_base(faculty_id, _simple_enrollments, _simple_allocation, _simple_workload)
 
 
 def _delete_cache_entry(fac_id):

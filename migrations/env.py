240a42aa69d33a0b437a79cf15bd44aad3ee45cf
ds283 <D.Seery@sussex.ac.uk>
2023-07-04 00:02:35 +0100

    Consolidate alembic migrations into a single new file
    - preparatory to deployment to Google Kubernetes Engine for EngInf rollout

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,9 @@
-from __future__ import with_statement
-from alembic import context
-from sqlalchemy import engine_from_config, pool
-from sqlalchemy.sql import sqltypes
-from logging.config import fileConfig
 import logging
+from logging.config import fileConfig
+
+from flask import current_app
+
+from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,14 +14,30 @@ config = context.config
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
 
+
+def get_engine():
+    try:
+        # this works with Flask-SQLAlchemy<3 and Alchemical
+        return current_app.extensions['migrate'].db.get_engine()
+    except TypeError:
+        # this works with Flask-SQLAlchemy>=3
+        return current_app.extensions['migrate'].db.engine
+
+
+def get_engine_url():
+    try:
+        return get_engine().url.render_as_string(hide_password=False).replace(
+            '%', '%%')
+    except AttributeError:
+        return str(get_engine().url).replace('%', '%%')
+
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from flask import current_app
-config.set_main_option('sqlalchemy.url',
-                       current_app.config.get('SQLALCHEMY_DATABASE_URI'))
-target_metadata = current_app.extensions['migrate'].db.metadata
+config.set_main_option('sqlalchemy.url', get_engine_url())
+target_db = current_app.extensions['migrate'].db
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -29,37 +45,10 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # ... etc.
 
 
-_compare_attrs = {
-    sqltypes._Binary: ('length', ),
-    sqltypes.Date: (),
-    sqltypes.DateTime: ('fsp', 'timezone'),
-    sqltypes.Integer: ('display_width', 'unsigned', 'zerofill'),
-    sqltypes.String: ('binary', 'charset', 'collation', 'length', 'unicode'),
-}
-
-
-def db_compare_type(context, inspected_column,
-                    metadata_column, inspected_type, metadata_type):
-    # return True if the types are different, False if not, or None
-    # to allow the default implementation to compare these TYPES
-    expected = metadata_column.type
-    migrated = inspected_column.type
-
-    # this extends the logic in alembic.ddl.impl.DefaultImpl.compare_type
-    type_affinity = migrated._type_affinity
-    compare_attrs = _compare_attrs.get(type_affinity, None)
-    if compare_attrs is not None:
-        if type(expected) != type(migrated):
-            return True
-        for attr in compare_attrs:
-            if getattr(expected, attr, None) != getattr(migrated, attr, None):
-                return True
-        return False
-
-    return None
-    #
-    # # fall back to limited alembic type comparison
-    # return context.impl.compare_type(expected, migrated)
+def get_metadata():
+    if hasattr(target_db, 'metadatas'):
+        return target_db.metadatas[None]
+    return target_db.metadata
 
 
 def run_migrations_offline():
@@ -75,7 +64,9 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+    context.configure(
+        url=url, target_metadata=get_metadata(), literal_binds=True
+    )
 
     with context.begin_transaction():
         context.run_migrations()
@@ -99,22 +90,19 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    engine = engine_from_config(config.get_section(config.config_ini_section),
-                                prefix='sqlalchemy.',
-                                poolclass=pool.NullPool)
+    connectable = get_engine()
 
-    connection = engine.connect()
-    context.configure(connection=connection,
-                      target_metadata=target_metadata,
-                      # compare_type=db_compare_type,
-                      process_revision_directives=process_revision_directives,
-                      **current_app.extensions['migrate'].configure_args)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=get_metadata(),
+            process_revision_directives=process_revision_directives,
+            **current_app.extensions['migrate'].configure_args
+        )
 
-    try:
         with context.begin_transaction():
             context.run_migrations()
-    finally:
-        connection.close()
+
 
 if context.is_offline_mode():
     run_migrations_offline()

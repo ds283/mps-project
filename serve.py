df2ac1f5d745bda0380d71e6c25a9a24832dd7e7
ds283 <D.Seery@sussex.ac.uk>
2023-07-04 11:43:58 +0100

    Add code to populate an empty database
    - uses SQL scripts from 6d8db562 to populate an empty database if the environment variable MPSPROJECT_POPULATE_DATABASE is set to 1
    
    - previously this type of code has been placed in mpsproject.py, but this has drawbacks. mpsproject.py used to generate the application object by the flask command, so it is loaded as part of alembic database migration. That means we would have a conflict between the migration process and the SQL command populating the database. It’s better to run the population logic in serve.py since this runs exactly once to launch the waitress webserver
    
    - also, previously, serve.py imported the app object from mpsproject.py, but there’s not really any point in doing that. We might as well create it directly here.

--- serve.py
+++ serve.py
@@ -8,7 +8,58 @@
 # Contributors: David Seery <D.Seery@sussex.ac.uk>
 #
 
+import os
+from datetime import datetime
+
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 from waitress import serve
-from mpsproject import app
+
+from app import create_app, db
+
+
+def populate_database():
+    print('!! EXECUTING POPULATE_DATABASE()')
+
+    scripts = ['basic_database/asset_licenses.sql',
+               'basic_database/celery_crontabs.sql',
+               'basic_database/celery_intervals.sql',
+               'basic_database/celery_schedules.sql',
+               'basic_database/main_config.sql',
+               'basic_database/roles.sql',
+               'basic_database/users.sql',
+               'basic_database/faculty_data.sql',
+               'basic_database/roles_users.sql']
+
+    now = datetime.now()
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    db.session.execute(text('SET FOREIGN_KEY_CHECKS = 0;'))
+
+    for script in scripts:
+        with open(script, 'r') as file:
+            while line := file.readline():
+                line_replace = line.replace('$$TIMESTAMP', now_str)
+                try:
+                    result = db.session.execute(text(line_replace))
+                except SQLAlchemyError as e:
+                    print('** Encountered exception while emplacing SQL line')
+                    print('     {line}'.format(line=line_replace))
+                    print(e)
+
+    db.session.execute(text('SET FOREIGN_KEY_CHECKS = 1;'))
+
+    db.session.commit()
+
+    print('== FINISHED POPULATE_DATABASE()')
+
+
+app = create_app()
+
+populate = os.getenv("MPSPROJECT_POPULATE_DATABASE")
+if populate is not None and populate == "1":
+    with app.app_context():
+        populate_database()
+
 
 serve(app, port=5000)

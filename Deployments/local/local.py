fa76aa37ccdc77daa87d91ae8954e849512a39f0
ds283 <D.Seery@sussex.ac.uk>
2023-08-30 23:20:37 +0100

    Update cached local.py for testing environments

--- Deployments/local/local.py
+++ Deployments/local/local.py
@@ -12,8 +12,22 @@ import os
 
 from app.shared.cloud_object_store import ObjectStore
 
-OBJECT_STORAGE_ASSETS_URI = os.environ.get("OBJECT_STORAGE_ASSETS_URI")
-OBJECT_STORAGE_ASSETS = ObjectStore(OBJECT_STORAGE_ASSETS_URI)
+APP_NAME = 'mpsprojects'
 
-OBJECT_STORAGE_BACKUP_URI = os.environ.get("OBJECT_STORAGE_BACKUP_URI")
-OBJECT_STORAGE_BACKUP = ObjectStore(OBJECT_STORAGE_BACKUP_URI)
+OBJECT_STORAGE_SERVICE_ACCOUNT_FILE = os.environ.get('OBJECT_STORAGE_SERVICE_ACCOUNT_FILE') or None
+_storage_options = {'google_service_account': OBJECT_STORAGE_SERVICE_ACCOUNT_FILE}
+
+OBJECT_STORAGE_ASSETS_URI = os.environ.get("OBJECT_STORAGE_ASSETS_URI") or 'file:///assets'
+OBJECT_STORAGE_ASSETS = ObjectStore(OBJECT_STORAGE_ASSETS_URI, _storage_options)
+
+OBJECT_STORAGE_BACKUP_URI = os.environ.get("OBJECT_STORAGE_BACKUP_URI") or 'file:///backup'
+OBJECT_STORAGE_BACKUP = ObjectStore(OBJECT_STORAGE_BACKUP_URI, _storage_options)
+
+BRANDING_LABEL = 'MPS projects portal'
+BRANDING_LOGIN_LANDING_STRING = 'Welcome to the MPS projects portal'
+BRANDING_PUBLIC_LANDING_STRING = 'Welcome to the MPS public projects list'
+
+ENABLE_PUBLIC_BROWSER = True
+
+VIDEO_EXPLAINER_PANOPTO_SERVER = 'sussex.cloud.panopto.eu'
+VIDEO_EXPLAINER_PANOPTO_SESSION = 'c2274660-a8f0-4f71-ba13-acd101735f7b'

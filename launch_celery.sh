5fd24efa5ba83da4de352450d01b4ad704020e2f
ds283 <D.Seery@sussex.ac.uk>
2023-08-30 10:35:16 +0100

    Pin Celery child workers to 4 processes
    - default is number of CPUs on the system, so when running on platforms with significant CPU availability this causes many child processes to start, and a lot of memory to be consumed. For example, this is an issue on the APPUiO Cloud deployment but was not previously visible on the ITS-hosted docker-compose

--- launch_celery.sh
+++ launch_celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A celery_node.celery worker -Ofair --loglevel=INFO --max-memory-per-child=500000 -Q ${WORKER_QUEUES}
+celery -A celery_node.celery worker -Ofair --loglevel=INFO -E --concurrency=4 --max-memory-per-child=500000 -Q ${WORKER_QUEUES}

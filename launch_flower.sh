2a71293ffdf9cc57c0de13d24c6ec14c1ce70d6f
ds283 <D.Seery@sussex.ac.uk>
2023-08-30 13:37:43 +0100

    Revert use of basic auth for flower; should migrate to other auth system such as GitHub

--- launch_flower.sh
+++ launch_flower.sh
@@ -8,4 +8,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-celery -A celery_node.celery flower --basic-auth=$FLOWER_USERNAME:$FLOWER_PASSWORD --port=5000 --scheduler app.sqlalchemy_scheduler:DatabaseScheduler --loglevel=info
+celery -A celery_node.celery flower --port=5000 --scheduler app.sqlalchemy_scheduler:DatabaseScheduler --loglevel=info

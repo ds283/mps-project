dc4a7ee67e951e5996b4e8c41d7c8fb1e984f63e
ds283 <D.Seery@sussex.ac.uk>
2023-09-05 11:06:04 +0100

    Untrack build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "git_tag='`git log -1 --format=%h`'" > app/build_data.py
-docker compose down
-docker compose build --force-rm
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-docker compose up -d

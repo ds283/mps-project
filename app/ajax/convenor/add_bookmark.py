ds283/mps-project

commit b01346c340ea5b75d3512a86d39fb37a5f96ca45

Refactor labelling implementation
- currently, a number of user interface elements (mostly labels) are generated in the backend, with the necessary UI markup being hardcoded. Mostly these relate to SQLAlchemy models that are aware how to generate their own context-sensitive labels, but there are a few other cases

- generally we don’t want to organize the UI like this. The backend should pass the necessary information to produce a label (and that can include formatting information in the form of hints, if necessary), but it should be up to the front end what it does with this information, and also exactly how the markup is implemented

- labelling functions in the backend now pass a dictionary to the Jinja2 layer that contains details of the label, including context information such as a colour or Bootstrap background class, but they don’t generate the UI markup themselves. This is now done in a “simple_label()” macro which currently renders everything as a badge, although we will be moving away from that in future to try to declutter the interface. However, different parts of the UI can now make different choices about exactly how these label elements are rendered

app/ajax/convenor/add_bookmark.py
@@ -25,7 +25,7 @@ _owner = \
 """
 {% if not project.generic and project.owner is not none %}
     <a class="text-decoration-none" href="mailto:{{ project.owner.user.email }}">{{ project.owner.user.name }}</a>
-    {% if project.group %}{{ project.group.make_label()|safe }}{% endif %}
+    {% if project.group %}{{ simple_label(project.group.make_label()) }}{% endif %}
 {% else %}
     <span class="badge bg-info">Generic</span>
 {% endif %}
@@ -45,9 +45,11 @@ _actions = \
 
 
 def add_student_bookmark(projects, sel):
+    simple_label = get_template_attribute("labels.html", "simple_label")
+
     data = [{'project': render_template_string(_project, sel=sel, proj=project),
              'owner': {
-                 'display': render_template_string(_owner, project=project),
+                 'display': render_template_string(_owner, project=project, simple_label=simple_label),
                  'sortvalue': project.owner.user.last_name + project.owner.user.first_name if not project.generic and project.owner is not None else 'Generic'
              },
              'actions': render_template_string(_actions, sel=sel, project=project)} for project in projects]

app/ajax/convenor/add_ranking.py
@@ -25,7 +25,7 @@ _owner = \
 """
 {% if not project.generic and project.owner is not none %}
     <a class="text-decoration-none" href="mailto:{{ project.owner.user.email }}">{{ project.owner.user.name }}</a>
-    {% if project.group %}{{ project.group.make_label()|safe }}{% endif %}
+    {% if project.group %}{{ simple_label(project.group.make_label()) }}{% endif %}
 {% else %}
     <span class="badge bg-info">Generic</span>
 {% endif %}
@@ -45,9 +45,11 @@ _actions = \
 
 
 def add_student_ranking(projects, sel):
+    simple_label = get_template_attribute("labels.html", "simple_label")
+
     data = [{'project': render_template_string(_project, sel=sel, proj=project),
              'owner': {
-                 'display': render_template_string(_owner, project=project),
+                 'display': render_template_string(_owner, project=project, simple_label=simple_label),
                  'sortvalue': project.owner.user.last_name + project.owner.user.first_name
              },
              'actions': render_template_string(_actions, sel=sel, project=project)} for project in projects]

commit f6a5129f0ec50064dbf831a001bf6a27112033cd

Fix minor errors introduced by b01346c3 and caught by code inspection

app/ajax/convenor/add_bookmark.py
@@ -8,8 +8,7 @@
 # Contributors: David Seery <D.Seery@sussex.ac.uk>
 #
 
-from flask import render_template_string, jsonify
-
+from flask import render_template_string, jsonify, get_template_attribute
 
 # language=jinja2
 _project = \

app/ajax/convenor/add_ranking.py
@@ -8,8 +8,7 @@
 # Contributors: David Seery <D.Seery@sussex.ac.uk>
 #
 
-from flask import render_template_string, jsonify
-
+from flask import render_template_string, jsonify, get_template_attribute
 
 # language=jinja2
 _project = \

app/models.py
@@ -3216,7 +3216,7 @@ class FacultyData(db.Model, EditingMetadataMixin):
             return {'label': 'Assessor for 0',
                     'type': 'secondary'}
 
-        return {'label': f'Assessor for {n}',
+        return {'label': f'Assessor for {num}',
                 'type': 'info'}
 
 
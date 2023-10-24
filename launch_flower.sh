4aea7a36f5d544823216e60c5dcbba2214e301ae
ds283 <D.Seery@sussex.ac.uk>
2023-10-24 13:16:13 +0100

    Remove --url-prefix parameter from flower launch script
    - This is better configured as an environment variable FLOWER_URL_PREFIX passed via docker compose or a Kubernetes environment, so it can be configured per-deployment

--- launch_flower.sh
+++ launch_flower.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A celery_node.celery flower --url-prefix=flower --port=5000 --scheduler app.sqlalchemy_scheduler:DatabaseScheduler --loglevel=info
+celery -A celery_node.celery flower --port=5000 --scheduler app.sqlalchemy_scheduler:DatabaseScheduler --loglevel=info

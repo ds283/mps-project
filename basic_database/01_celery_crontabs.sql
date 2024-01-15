9e4d142ae81fefcef115200530c71d76e338c91a
ds283 <D.Seery@sussex.ac.uk>
2024-01-15 13:44:52 +0000

    Update basic database to include Cloud API telemetry task

--- basic_database/01_celery_crontabs.sql
+++ basic_database/01_celery_crontabs.sql
@@ -1,1 +1,2 @@
 INSERT INTO celery_crontabs (id, minute, hour, day_of_week, day_of_month, month_of_year) VALUES (1, '00', '16', 'Mon,Tue,Wed,Thu,Fri', '*', '*');
+INSERT INTO celery_crontabs (id, minute, hour, day_of_week, day_of_month, month_of_year) VALUES (2, '0', '8', '*', '1', '*');

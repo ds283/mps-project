f8a3bd5b5890fb9d187bd4853e39e436dba4cdce
ds283 <D.Seery@sussex.ac.uk>
2024-03-25 12:32:43 +0000

    Remove debugging code

--- app/models.py
+++ app/models.py
@@ -6223,7 +6223,6 @@ class ProjectClassConfig(db.Model, ConvenorTasksMixinFactory(ConvenorGenericTask
 
                     data.update({"compare": compare, "delta": delta})
 
-            print(f'Project "{p.name}", data={data}')
             return data
 
         items = [_build_item(*p) for p in query]

1ecca3d1bf2ab5637a5fb44d68a2e89185e50676
ds283 <D.Seery@sussex.ac.uk>
2025-06-30 01:48:15 +0100

    Temporarily disable marker assignment checks (again)

--- app/convenor/forms.py
+++ app/convenor/forms.py
@@ -487,12 +487,13 @@ class AddSubmitterRoleForm(Form):
 
         if project is not None:
             if role_type == SubmissionRole.ROLE_MARKER:
-                if fd not in project.assessor_list:
-                    raise ValidationError(
-                        'User "{name}" is not in the assessor pool for the assigned project. '
-                        "Please select a different user to attach for this role, or select "
-                        "a different role type.".format(name=fd.user.name)
-                    )
+                pass
+                # if fd not in project.assessor_list:
+                #     raise ValidationError(
+                #         'User "{name}" is not in the assessor pool for the assigned project. '
+                #         "Please select a different user to attach for this role, or select "
+                #         "a different role type.".format(name=fd.user.name)
+                #     )
 
             if role_type in [SubmissionRole.ROLE_SUPERVISOR, SubmissionRole.ROLE_RESPONSIBLE_SUPERVISOR]:
                 if project.generic:

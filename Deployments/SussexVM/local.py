fd11815baeeaf4580d414192789ab170c89d1fc1
ds283 <D.Seery@sussex.ac.uk>
2026-04-18 23:56:20 +0100

    Update some details of SussexVM deployment

--- Deployments/SussexVM/local.py
+++ Deployments/SussexVM/local.py
@@ -16,12 +16,12 @@ from app.shared.cloud_object_store import ObjectStore
 from app.shared.cloud_object_store.encryption.chacha20_poly1305 import ChaCha20_Poly1305
 
 # APP CONFIGURATION
-APP_NAME = 'mpsprojects'
+APP_NAME = "mpsprojects"
 
 # branding labels
-BRANDING_LABEL = 'MPS projects management'
-BRANDING_LOGIN_LANDING_STRING = 'Welcome to the MPS projects portal'
-BRANDING_PUBLIC_LANDING_STRING = 'Welcome to the MPS public projects list'
+BRANDING_LABEL = "MPS projects management"
+BRANDING_LOGIN_LANDING_STRING = "Welcome to the MPS projects portal"
+BRANDING_PUBLIC_LANDING_STRING = "Welcome to the MPS public projects list"
 
 # public browser
 ENABLE_PUBLIC_BROWSER = True
@@ -33,7 +33,7 @@ EMAIL_IS_LIVE = True
 
 # FLASK
 
-PREFERRED_URL_SCHEME = 'https'
+PREFERRED_URL_SCHEME = "https"
 
 
 # CLOUD API AUDIT
@@ -41,8 +41,12 @@ PREFERRED_URL_SCHEME = 'https'
 # get cloud API audit configuration
 OBJECT_STORAGE_AUDIT_API = bool(int(os.environ.get("OBJECT_STORAGE_AUDIT_API", 0)))
 OBJECT_STORAGE_AUDIT_BACKEND_URI = os.environ.get("OBJECT_STORAGE_AUDIT_BACKEND_URI")
-OBJECT_STORAGE_AUDIT_BACKEND_DATABASE = os.environ.get("OBJECT_STORAGE_AUDIT_BACKEND_DATABASE")
-OBJECT_STORAGE_AUDIT_BACKEND_COLLECTION = os.environ.get("OBJECT_STORAGE_AUDIT_BACKEND_COLLECTION")
+OBJECT_STORAGE_AUDIT_BACKEND_DATABASE = os.environ.get(
+    "OBJECT_STORAGE_AUDIT_BACKEND_DATABASE"
+)
+OBJECT_STORAGE_AUDIT_BACKEND_COLLECTION = os.environ.get(
+    "OBJECT_STORAGE_AUDIT_BACKEND_COLLECTION"
+)
 
 
 # OBJECT BUCKETS
@@ -80,7 +84,9 @@ _assets_storage_options = _base_storage_options | {
 
 # create ObjectStore for assets bucket
 OBJECT_STORAGE_ASSETS_URI = os.environ.get("OBJECT_STORAGE_ASSETS_URI")
-OBJECT_STORAGE_ASSETS = ObjectStore(OBJECT_STORAGE_ASSETS_URI, buckets.ASSETS_BUCKET, _assets_storage_options)
+OBJECT_STORAGE_ASSETS = ObjectStore(
+    OBJECT_STORAGE_ASSETS_URI, buckets.ASSETS_BUCKET, _assets_storage_options
+)
 
 # -- BACKUP BUCKET
 
@@ -101,13 +107,19 @@ _backup_storage_options = _base_storage_options | {
 
 # create ObjectStore for backup bucket
 OBJECT_STORAGE_BACKUP_URI = os.environ.get("OBJECT_STORAGE_BACKUP_URI")
-OBJECT_STORAGE_BACKUP = ObjectStore(OBJECT_STORAGE_BACKUP_URI, buckets.BACKUP_BUCKET, _backup_storage_options)
+OBJECT_STORAGE_BACKUP = ObjectStore(
+    OBJECT_STORAGE_BACKUP_URI, buckets.BACKUP_BUCKET, _backup_storage_options
+)
 
 # -- TELEMETRY BUCKET
 
 # get credentials to access telemetry bucket
-OBJECT_STORAGE_TELEMETRY_ACCESS_KEY = os.environ.get("OBJECT_STORAGE_TELEMETRY_ACCESS_KEY")
-OBJECT_STORAGE_TELEMETRY_SECRET_KEY = os.environ.get("OBJECT_STORAGE_TELEMETRY_SECRET_KEY")
+OBJECT_STORAGE_TELEMETRY_ACCESS_KEY = os.environ.get(
+    "OBJECT_STORAGE_TELEMETRY_ACCESS_KEY"
+)
+OBJECT_STORAGE_TELEMETRY_SECRET_KEY = os.environ.get(
+    "OBJECT_STORAGE_TELEMETRY_SECRET_KEY"
+)
 
 _telemetry_storage_options = _base_storage_options | {
     "access_key": OBJECT_STORAGE_TELEMETRY_ACCESS_KEY,
@@ -117,16 +129,24 @@ _telemetry_storage_options = _base_storage_options | {
 
 # create ObjectStore for telemetry bucket
 OBJECT_STORAGE_TELEMETRY_URI = os.environ.get("OBJECT_STORAGE_TELEMETRY_URI")
-OBJECT_STORAGE_TELEMETRY = ObjectStore(OBJECT_STORAGE_TELEMETRY_URI, buckets.TELEMETRY_BUCKET, _telemetry_storage_options)
+OBJECT_STORAGE_TELEMETRY = ObjectStore(
+    OBJECT_STORAGE_TELEMETRY_URI, buckets.TELEMETRY_BUCKET, _telemetry_storage_options
+)
 
 # -- FEEDBACK BUCKET
 
 # get credentials to access feedback bucket
-OBJECT_STORAGE_FEEDBACK_ACCESS_KEY = os.environ.get("OBJECT_STORAGE_FEEDBACK_ACCESS_KEY")
-OBJECT_STORAGE_FEEDBACK_SECRET_KEY = os.environ.get("OBJECT_STORAGE_FEEDBACK_SECRET_KEY")
+OBJECT_STORAGE_FEEDBACK_ACCESS_KEY = os.environ.get(
+    "OBJECT_STORAGE_FEEDBACK_ACCESS_KEY"
+)
+OBJECT_STORAGE_FEEDBACK_SECRET_KEY = os.environ.get(
+    "OBJECT_STORAGE_FEEDBACK_SECRET_KEY"
+)
 
 # set up encryption pipeline for feedback bucket
-OBJECT_STORAGE_FEEDBACK_ENCRYPT_KEY = os.environ.get("OBJECT_STORAGE_FEEDBACK_ENCRYPT_KEY")
+OBJECT_STORAGE_FEEDBACK_ENCRYPT_KEY = os.environ.get(
+    "OBJECT_STORAGE_FEEDBACK_ENCRYPT_KEY"
+)
 _feedback_encrypt_key = base64.urlsafe_b64decode(OBJECT_STORAGE_FEEDBACK_ENCRYPT_KEY)
 
 _feedback_storage_options = _base_storage_options | {
@@ -137,7 +157,9 @@ _feedback_storage_options = _base_storage_options | {
 
 # create ObjectStore for feedback bucket
 OBJECT_STORAGE_FEEDBACK_URI = os.environ.get("OBJECT_STORAGE_FEEDBACK_URI")
-OBJECT_STORAGE_FEEDBACK = ObjectStore(OBJECT_STORAGE_FEEDBACK_URI, buckets.FEEDBACK_BUCKET, _feedback_storage_options)
+OBJECT_STORAGE_FEEDBACK = ObjectStore(
+    OBJECT_STORAGE_FEEDBACK_URI, buckets.FEEDBACK_BUCKET, _feedback_storage_options
+)
 
 # -- PROJECT BUCKET
 
@@ -146,7 +168,9 @@ OBJECT_STORAGE_PROJECT_ACCESS_KEY = os.environ.get("OBJECT_STORAGE_PROJECT_ACCES
 OBJECT_STORAGE_PROJECT_SECRET_KEY = os.environ.get("OBJECT_STORAGE_PROJECT_SECRET_KEY")
 
 # set up encryption pipeline for project bucket
-OBJECT_STORAGE_PROJECT_ENCRYPT_KEY = os.environ.get("OBJECT_STORAGE_PROJECT_ENCRYPT_KEY")
+OBJECT_STORAGE_PROJECT_ENCRYPT_KEY = os.environ.get(
+    "OBJECT_STORAGE_PROJECT_ENCRYPT_KEY"
+)
 _project_encrypt_key = base64.urlsafe_b64decode(OBJECT_STORAGE_PROJECT_ENCRYPT_KEY)
 
 _project_storage_options = _base_storage_options | {
@@ -157,4 +181,75 @@ _project_storage_options = _base_storage_options | {
 
 # create ObjectStore for project bucket
 OBJECT_STORAGE_PROJECT_URI = os.environ.get("OBJECT_STORAGE_PROJECT_URI")
-OBJECT_STORAGE_PROJECT = ObjectStore(OBJECT_STORAGE_PROJECT_URI, buckets.PROJECT_BUCKET, _project_storage_options)
+OBJECT_STORAGE_PROJECT = ObjectStore(
+    OBJECT_STORAGE_PROJECT_URI, buckets.PROJECT_BUCKET, _project_storage_options
+)
+
+# -- SUPERVISION ASSETS BUCKET
+
+# get credentials to access supervision assets bucket
+OBJECT_STORAGE_SUPERVISION_ASSETS_ACCESS_KEY = os.environ.get(
+    "OBJECT_STORAGE_SUPERVISION_ASSETS_ACCESS_KEY"
+)
+OBJECT_STORAGE_SUPERVISION_ASSETS_SECRET_KEY = os.environ.get(
+    "OBJECT_STORAGE_SUPERVISION_ASSETS_SECRET_KEY"
+)
+
+# set up encryption pipeline for supervision assets bucket
+OBJECT_STORAGE_SUPERVISION_ASSETS_ENCRYPT_KEY = os.environ.get(
+    "OBJECT_STORAGE_SUPERVISION_ASSETS_ENCRYPT_KEY"
+)
+_supervision_assets_encrypt_key = base64.urlsafe_b64decode(
+    OBJECT_STORAGE_SUPERVISION_ASSETS_ENCRYPT_KEY
+)
+
+_supervision_assets_storage_options = _base_storage_options | {
+    "access_key": OBJECT_STORAGE_SUPERVISION_ASSETS_ACCESS_KEY,
+    "secret_key": OBJECT_STORAGE_SUPERVISION_ASSETS_SECRET_KEY,
+    "encryption_pipeline": ChaCha20_Poly1305(_supervision_assets_encrypt_key),
+}
+
+# create ObjectStore for supervision assets bucket
+OBJECT_STORAGE_SUPERVISION_ASSETS_URI = os.environ.get(
+    "OBJECT_STORAGE_SUPERVISION_ASSETS_URI"
+)
+OBJECT_STORAGE_SUPERVISION_ASSETS = ObjectStore(
+    OBJECT_STORAGE_SUPERVISION_ASSETS_URI,
+    buckets.SUPERVISION_ASSETS_BUCKET,
+    _supervision_assets_storage_options,
+)
+
+# -- THUMBNAILS ASSETS BUCKET
+
+# get credentials to access thumbnails assets bucket
+OBJECT_STORAGE_THUMBNAILS_ACCESS_KEY = os.environ.get(
+    "OBJECT_STORAGE_THUMBNAILS_ACCESS_KEY"
+)
+OBJECT_STORAGE_THUMBNAILS_SECRET_KEY = os.environ.get(
+    "OBJECT_STORAGE_THUMBNAILS_SECRET_KEY"
+)
+
+_thumbnails_assets_storage_options = _base_storage_options | {
+    "access_key": OBJECT_STORAGE_THUMBNAILS_ACCESS_KEY,
+    "secret_key": OBJECT_STORAGE_THUMBNAILS_SECRET_KEY,
+    "compressed": False,
+}
+
+# create ObjectStore for thumbnails assets bucket
+OBJECT_STORAGE_THUMBNAILS_URI = os.environ.get("OBJECT_STORAGE_THUMBNAILS_URI")
+OBJECT_STORAGE_THUMBNAILS_ASSETS = ObjectStore(
+    OBJECT_STORAGE_THUMBNAILS_URI,
+    buckets.THUMBNAILS_BUCKET,
+    _thumbnails_assets_storage_options,
+)
+
+# set up bucket map
+OBJECT_STORAGE_BUCKETS = {
+    buckets.ASSETS_BUCKET: OBJECT_STORAGE_ASSETS,
+    buckets.BACKUP_BUCKET: OBJECT_STORAGE_BACKUP,
+    buckets.TELEMETRY_BUCKET: OBJECT_STORAGE_TELEMETRY,
+    buckets.FEEDBACK_BUCKET: OBJECT_STORAGE_FEEDBACK,
+    buckets.PROJECT_BUCKET: OBJECT_STORAGE_PROJECT,
+    buckets.SUPERVISION_ASSETS_BUCKET: OBJECT_STORAGE_SUPERVISION_ASSETS,
+    buckets.THUMBNAILS_BUCKET: OBJECT_STORAGE_THUMBNAILS_ASSETS,
+}
